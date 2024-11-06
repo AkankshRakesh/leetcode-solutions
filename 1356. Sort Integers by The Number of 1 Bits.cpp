914b9803f160770e7d6bf7f78c24194f2b149c34
AkankshRakesh <akankshrakesh@gmail.com>
2024-11-06 09:40:00 +0530

    Time complexity: O(NlogN), Space complexity: O(1)

---
+++ 1356. Sort Integers by The Number of 1 Bits.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    static int count(int n){
+        int c = 0;
+        while(n){
+            n &= (n - 1);
+            c++;
+        }
+        return c;
+    }
+    static bool compare(int a, int b){
+        int c1 = count(a);
+        int c2 = count(b);
+
+        if(c1 == c2){
+            return a < b;
+        }
+        return c1 < c2;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
