52d3bfd81b9fdff3c748aabb944e3bb87eb7e42f
AkankshRakesh <akankshrakesh@gmail.com>
2025-01-11 15:45:52 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 1400. Construct K Palindrome Strings.c
@@ -0,0 +1,25 @@
+bool canConstruct(char *s, int k) {
+    int n = strlen(s);
+    if (n < k) {
+        return false;
+    } else if (n == k) {
+        return true;
+    }
+
+    int numArr[26] = {0};
+    for (int i = 0; i < n; i++) {
+        numArr[s[i] - 'a']++;
+    }
+
+    int odd = 0;
+    for (int i = 0; i < 26; i++) {
+        if (numArr[i] % 2 != 0) {
+            odd++;
+        }
+    }
+
+    if (odd > k) {
+        return false;
+    }
+    return true;
+}
