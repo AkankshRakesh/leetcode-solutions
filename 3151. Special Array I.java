8c39e334207157b0b6fef011ee65264cdc9edefc
AkankshRakesh <akankshrakesh@gmail.com>
2025-02-01 09:52:50 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 3151. Special Array I.java
+++ 3151. Special Array I.java
@@ -1,14 +1,14 @@
 class Solution {
-    public:
-        bool isArraySpecial(vector<int>& nums) {
-            bool nextEven = true;
-            if(nums[0] % 2 == 0) nextEven = false;
-            int n = nums.size();
-            for(int i = 1; i < n; i++){
-                if(!nextEven && nums[i] % 2 == 0) return false;
-                else if(nextEven && (nums[i] % 2 != 0 || nums[i] == 1)) return false;
-                nextEven = !nextEven;
-            }
-            return true;
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        boolean nextEven = true;
+        if(nums[0] % 2 == 0) nextEven = false;
+
+        for(int i = 1; i < n; i++){
+            if(nextEven && nums[i] % 2 != 0) return false;
+            else if(!nextEven && nums[i] % 2 == 0) return false;
+            nextEven = !nextEven;
         }
-    };
+        return true;
+    }
+}
