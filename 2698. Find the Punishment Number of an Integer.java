a817f43d7fc523acce30e758efaebd5b8372dd4c
AkankshRakesh <akankshrakesh@gmail.com>
2025-02-15 08:11:42 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 2698. Find the Punishment Number of an Integer.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public boolean canPartition(int num, int target) {
+        if (target < 0 || num < target) {
+            return false;
+        }
+
+        if (num == target) {
+            return true;
+        }
+
+        return (
+            canPartition(num / 10, target - (num % 10)) ||
+            canPartition(num / 100, target - (num % 100)) ||
+            canPartition(num / 1000, target - (num % 1000))
+        );
+    }
+
+    public int punishmentNumber(int n) {
+        int punishmentNum = 0;
+
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+            if(currentNum % 9 != 0 && currentNum % 9 != 1) continue;
+            if (canPartition(squareNum, currentNum)) {
+                punishmentNum += squareNum;
+            }
+        }
+        return punishmentNum;
+    }
+}
