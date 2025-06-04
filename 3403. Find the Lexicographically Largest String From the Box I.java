50d68c1e7088dfc4b0c025dbbb097ab102ff80cb
AkankshRakesh <akankshrakesh@gmail.com>
2025-06-04 08:49:09 +0530

    Time complexity: O(n^2), Space complexity: O(n)

---
+++ 3403. Find the Lexicographically Largest String From the Box I.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String answerString(String word, int numFriends) {
+        if(numFriends == 1) return word;
+        int n = word.length();
+        String ans = "";
+
+        int len = n - numFriends + 1;
+        
+        for(int i = 0; i < n; i++){
+            String temp;
+            if(i + len <= word.length()) temp = word.substring(i, i + len);
+            else temp = word.substring(i);
+
+            if(temp.compareTo(ans) > 0) ans = temp;
+        }
+        return ans;
+    }
+}
