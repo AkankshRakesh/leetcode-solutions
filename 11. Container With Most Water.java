58ee54c7101f951e3538a0b4456af09ef03ebae7
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-11 09:42:12 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 11. Container With Most Water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while(left < right){
+            if(height[left] > height[right]){
+                int area = height[right] * (right - left);
+                if(area > maxArea) maxArea = area;
+                right--;
+            }
+            else{
+                int area = height[left] * (right - left);
+                if(area > maxArea) maxArea = area;
+                left++;
+            }
+        }
+
+        return maxArea;
+    }
+}
