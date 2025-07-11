da595779d691208a799596b00f36d5a291d4d439
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-11 10:04:33 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int profit = 0;
+        int n = prices.size();
+
+        int currStock = prices[0];
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] > currStock) {
+                profit += prices[i] - currStock;
+            }
+            currStock = prices[i];
+        }
+
+        return profit;
+    }
+};
