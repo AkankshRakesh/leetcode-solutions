76cd4ae98eae36657da0b283b76b0beb06b97ed2
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-12 15:17:52 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int max = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
+            if(sum < 0) sum = 0;
+            sum += nums[i];
+            if(max < sum) max = sum;
+        }
+        return max;
+    }
+};
