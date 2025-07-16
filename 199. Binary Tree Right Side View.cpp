b22c9e5e03459ca9e71b5ada667e2466e69f72d1
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-16 11:55:27 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 199. Binary Tree Right Side View.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> levelQue;
+        levelQue.push(root);
+        vector<int> res;
+        if(root == NULL) return res;
+        res.push_back(root -> val);
+        while(!levelQue.empty()){
+            int len = levelQue.size();
+            for(int i = 0; i < len; i++){
+                TreeNode* node = levelQue.front();
+                levelQue.pop();
+                if(node -> left) levelQue.push(node -> left);
+                if(node -> right) levelQue.push(node -> right);
+            }
+            if(!levelQue.empty())res.push_back(levelQue.back() -> val);
+        }
+        return res;
+    }
+};
