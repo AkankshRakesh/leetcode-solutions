1c00fec61af810c87b95533b48941d9a551021b6
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-18 13:22:15 +0530

    Time complexity: O(n), Space complexity: O(logn)

---
+++ 108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* buildBST(vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+
+        int mid = left + (right - left) / 2;
+        TreeNode* node = new TreeNode(nums[mid]);
+
+        node->left = buildBST(nums, left, mid - 1);
+        node->right = buildBST(nums, mid + 1, right);
+
+        return node;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return buildBST(nums, 0, nums.size() - 1);
+    }
+};
