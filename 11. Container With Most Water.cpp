3b2399fd5e971c379ba89726b8207a57a7c3099e
AkankshRakesh <akankshrakesh@gmail.com>
2025-07-31 19:30:28 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 11. Container With Most Water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxSize = 0;
+
+        while (left < right) {
+            int dist = right - left;
+            if (height[left] < height[right]) {
+                maxSize = max(maxSize, height[left] * dist);
+                left++;
+            } else {
+                maxSize = max(maxSize, height[right] * dist);
+                right--;
+            }
+        }
+        return maxSize;
+    }
+};
