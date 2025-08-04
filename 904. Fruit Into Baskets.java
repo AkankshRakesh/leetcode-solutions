acb2b534959f7ddab5372a54c28d107df19276ed
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-04 11:35:00 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 904. Fruit Into Baskets.java
+++ 904. Fruit Into Baskets.java
@@ -1,22 +1,25 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-        int left = 0, maxLen = 0;
-
-        for (int right = 0; right < fruits.length; right++) {
+        int left = 0;
+        int n = fruits.length;
+        if(n == 1) return 1;
+        hm.put(fruits[0], 1);
+        
+        int maxSize = 1;
+        for(int right = 1; right < n; right++){
             hm.put(fruits[right], hm.getOrDefault(fruits[right], 0) + 1);
 
-            while (hm.size() > 2) {
-                hm.put(fruits[left], hm.get(fruits[left]) - 1);
-                if (hm.get(fruits[left]) == 0) {
-                    hm.remove(fruits[left]);
-                }
+            while(hm.size() > 2){
+                int count = hm.get(fruits[left]);
+                if(count == 1) hm.remove(fruits[left]);
+                else hm.put(fruits[left], count - 1);
                 left++;
             }
 
-            maxLen = Math.max(maxLen, right - left + 1);
+            maxSize = maxSize > right - left + 1 ? maxSize : right - left + 1;
         }
 
-        return maxLen;
+        return maxSize;
     }
 }
