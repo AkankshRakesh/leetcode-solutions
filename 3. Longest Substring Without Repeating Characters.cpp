19a9c23171eec77e0687a5ae2f0556bd87b191dd
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-06 19:24:57 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        unordered_set<char> hs;
+        int left = 0, ans = 0;
+
+        for (int right = 0; right < n; ++right) {
+            while (hs.find(s[right]) != hs.end()) {
+                hs.erase(s[left]);
+                ++left;
+            }
+            hs.insert(s[right]);
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
