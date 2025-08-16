da8acba6a0934ae04df568f561429b5947b58b2d
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-16 12:50:38 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,20 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
+    ListNode* rev(ListNode* head, ListNode* prev) {
         if (head == nullptr) return head;
-        
-        ListNode* next = head->next;
-        ListNode* prev = nullptr;
-
-        while (next != nullptr) {
+        if (head->next == nullptr) {
             head->next = prev;
-            ListNode* temp = next->next;
-            next->next = head;
-            prev = head;
-            head = next;
-            next = temp;
+            return head;
         }
-
-        return head;
+        ListNode* next = head->next;
+        head->next = prev;
+        return rev(next, head);
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        return rev(head, nullptr);
     }
 };
