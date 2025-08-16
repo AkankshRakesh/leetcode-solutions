5f88f56544bd2fed2680361bfd3353fe1dbe9442
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-16 11:59:06 +0530

    Time complexity: O(logn), Space complexity: O(1)

--- 202. Happy Number.cpp
+++ 202. Happy Number.cpp
@@ -1,19 +1,21 @@
 class Solution {
-public:
-    bool isHappy(int n) {
+    public boolean isHappy(int n) {
         if(n == 1) return true;
-        int arr[10] = {0,1, 4, 9, 16, 25, 36, 49, 64, 81};
-        std::unordered_set<int> seen;
-        while(n != 1 && seen.find(n) == seen.end()){
-            seen.insert(n);
-            int sum = 0, squareSum = 0;
-            while(n){
-                squareSum += arr[n % 10];
-                sum += n % 10;
+
+        HashSet<Integer> hs = new HashSet<>();
+        while(!hs.contains(n)){
+            if(n == 1) return true;
+
+            hs.add(n);
+            int next = 0;
+            while(n != 0){
+                next += ((n % 10) * (n % 10));
                 n /= 10;
             }
-            n = squareSum;
+
+            n = next;
         }
-        return n==1;
+
+        return false;
     }
-};
+}
