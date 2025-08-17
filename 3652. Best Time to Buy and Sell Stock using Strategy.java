9aa8f2af9a7a17d004a8ac7eff764463a4876cd5
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-17 09:40:44 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 3652. Best Time to Buy and Sell Stock using Strategy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+        long original = 0;
+        for (int i = 0; i < n; i++)
+            original += (long) prices[i] * strategy[i];
+
+        long[] buyToHold = new long[n + 1];
+        long[] holdToSell = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            buyToHold[i + 1] = buyToHold[i] - (long) strategy[i] * prices[i];
+            holdToSell[i + 1] = holdToSell[i] + ((long) 1 - strategy[i]) * prices[i];
+        }
+
+        long maxGain = 0;
+
+        for (int start = 0; start <= n - k; start++) {
+            int mid = start + k / 2;
+            int end = start + k;
+
+            long gain = (buyToHold[mid] - buyToHold[start]) + (holdToSell[end] - holdToSell[mid]);
+            if (gain > maxGain)
+                maxGain = gain;
+        }
+
+        return original + maxGain;
+    }
+}
