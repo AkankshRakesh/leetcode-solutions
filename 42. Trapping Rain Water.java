045c6a4da6ee5ad5a1690b9622ec2e2c8a36e493
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-19 19:11:17 +0530

    Time complexity: O(n, Space complexity: O(1)

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -1,28 +1,35 @@
 class Solution {
-    public int trap(int[] nums) {
-        int left = 0;
-        int n = nums.length;
-        int right = n - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-        while (left < right) {
-            if (nums[left] < nums[right]) {
-                if (nums[left] >= leftMax) {
-                    leftMax = nums[left];
-                } else {
-                    water += leftMax - nums[left];
-                }
-                left++;
-            } else {
-                if (nums[right] >= rightMax) {
-                    rightMax = nums[right];
-                } else {
-                    water += rightMax - nums[right];
-                }
-                right--;
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] fromLeft = new int[n];
+        int[] fromRight = new int[n];
+        fromLeft[0] = 0;
+        fromRight[n - 1] = 0;
+
+        int max = height[0];
+        for(int i = 1; i < n; i++){
+            if(height[i] > max){
+                fromLeft[i] = 0;
+                max = height[i];
             }
+            else fromLeft[i] = max - height[i];
         }
 
-        return water;
+        max = height[n - 1];
+        for(int i = n - 1; i >= 0; i--){
+            if(height[i] > max){
+                fromRight[i] = 0;
+                max = height[i];
+            }
+            else fromRight[i] = max - height[i];
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(fromLeft[i] == 0 || fromRight[i] == 0) continue;
+            ans += Math.min(fromLeft[i], fromRight[i]);
+        }
+
+        return ans;
     }
 }
