2193f10cc34456353d16f00d9ec5497ba966d11b
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-20 19:52:42 +0530

    Time complexity: O(2^n * n), Space complexity: O(2^n * n)

--- 90. Subsets II.cpp
+++ 90. Subsets II.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums, int index, vector<int>& currArr,
-                   vector<vector<int>>& arr, int n, set<vector<int>>& hs) {
-        if (index == n) {
-            if (hs.find(currArr) == hs.end()) {
-                arr.push_back(currArr);
-                hs.insert(currArr);
-            }
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, int ind, vector<int>& currArr, set<vector<int>>& hs) {
+        if (ind >= nums.size()) {
+            if (hs.find(currArr) != hs.end()) return;
+            hs.insert(currArr);
+            res.push_back(currArr);
             return;
         }
 
-        currArr.push_back(nums[index]);
-        backtrack(nums, index + 1, currArr, arr, n, hs);
+        currArr.push_back(nums[ind]);
+        backtrack(nums, res, ind + 1, currArr, hs);
 
         currArr.pop_back();
-
-        backtrack(nums, index + 1, currArr, arr, n, hs);
+        backtrack(nums, res, ind + 1, currArr, hs);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> arr;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        vector<vector<int>> res;
         set<vector<int>> hs;
-        vector<int> currArr;
-        sort(nums.begin(), nums.end()); // Important to avoid duplicate permutations
-        backtrack(nums, 0, currArr, arr, nums.size(), hs);
-        return arr;
+
+        res.push_back({});
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || (i > 0 && nums[i - 1] != nums[i])) {
+                vector<int> temp;
+                temp.push_back(nums[i]);
+                backtrack(nums, res, i + 1, temp, hs);
+            }
+        }
+
+        return res;
     }
 };
