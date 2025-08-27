d6a21d5da5b107d6e5acdcd106358b3736d859c9
AkankshRakesh <akankshrakesh@gmail.com>
2025-08-27 10:50:22 +0530

    Time complexity: O(n*m), Space complexity: O(n*m)

---
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int bfs(TreeNode node, int[] sum){
+        if(node == null) return 0;
+
+        int left = Math.max(0, bfs(node.left, sum));
+        int right = Math.max(0, bfs(node.right, sum));
+        
+        sum[0] = Math.max(sum[0], left + right + node.val);
+
+        return Math.max(left, right) + node.val;
+    }
+    public int maxPathSum(TreeNode root) {
+        int[] sum = { root.val };
+        bfs(root, sum);
+        return sum[0];
+    }
+}
