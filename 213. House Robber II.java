3df9c7bf1f560ade821677c2d6b6b8ba937f855a
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-03 20:49:27 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 213. House Robber II.java
+++ 213. House Robber II.java
@@ -1,22 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0], nums[1]);
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]); 
+        int[] dp = new int[n];
+        int ans = 0;
 
-        int max1 = robLinear(nums, 0, n - 2);
-        int max2 = robLinear(nums, 1, n - 1);
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[1], dp[0]);
+        for(int i = 2; i < n - 1; i++) dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        ans = dp[n - 2];
+        dp = new int[n];
+        dp[1] = nums[1];
+        dp[2] = Math.max(nums[1], nums[2]);
+        for(int i = 3; i < n; i++) dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
 
-        return Math.max(max1, max2);
-    }
-
-    private int robLinear(int[] nums, int start, int end) {
-        int prev1 = 0, prev2 = 0;
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = temp;
-        }
-        return prev1;
+        return Math.max(dp[n - 1], ans);
     }
 }
