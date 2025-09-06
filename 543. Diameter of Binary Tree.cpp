ff0f88b9d9562f5f0d9c05365dabe34990ae6dac
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-06 19:18:47 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 543. Diameter of Binary Tree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *leftNode, TreeNode *rightNode)
+//         : val(x), left(leftNode), right(rightNode) {}
+// };
+
+class Solution {
+public:
+    int ans = INT_MIN;
+
+    int find(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = find(root->left);
+        int right = find(root->right);
+
+        ans = max(ans, 1 + left + right);
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        find(root);
+        return ans - 1;
+    }
+};
