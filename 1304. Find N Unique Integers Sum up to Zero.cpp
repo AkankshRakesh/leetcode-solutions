f15e275eb329dfe6d22c7c6ede28caf2c98efaf2
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-07 09:26:39 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 1304. Find N Unique Integers Sum up to Zero.cpp
+++ 1304. Find N Unique Integers Sum up to Zero.cpp
@@ -1,32 +1,20 @@
 class Solution {
-    public int[] sumZero(int n) {
-        int[] ans = new int[n];
-        if(n == 1){
-            ans[0] = 0;
-            return ans;
+public:
+    vector<int> sumZero(int n) {
+        if (n == 1) return {0};
+
+        int num = n;
+        vector<int> ans(n);
+        int len = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            ans[len++] = -num;
+            ans[len++] = num;
+            num--;
         }
-        int ind = 0;
-        if(n % 2 == 0){
-            int temp = n;
-            while(ind != n){
-                ans[ind++] = temp;
-                ans[ind++] = -temp;
-                temp--;
-            }
-        }
-        else{
-            int temp = n;
-            ans[ind++] = -temp;
-            ans[ind++] = temp - 1;
-            ans[ind++] = 1;
-            temp -= 2;
-            while(ind != n){
-                ans[ind++] = temp;
-                ans[ind++] = -temp;
-                temp--;
-            }
-        } 
+
+        if (n % 2 != 0) ans[len] = 0;
 
         return ans;
     }
-}
+};
