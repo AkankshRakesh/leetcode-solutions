3aa27431007037a695fcd24b6dd938daac9feea0
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-08 17:00:41 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        reverse(nums, 0, n - 1);
+
+        k = k % n;
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+};
