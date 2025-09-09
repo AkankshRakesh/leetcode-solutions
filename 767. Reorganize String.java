08591218d0db97ce7d098448931e7c5911d2a43f
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-09 15:52:13 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 767. Reorganize String.java
+++ 767. Reorganize String.java
@@ -1,51 +1,43 @@
 class Solution {
     class Pair{
+        Character ch;
         Integer count;
-        Character letter;
-        public Pair(Character letter, Integer count){
+        public Pair(Character ch, Integer count){
+            this.ch = ch;
             this.count = count;
-            this.letter = letter;
         }
     }
     public String reorganizeString(String s) {
-        StringBuilder sb = new StringBuilder();
+        if(s.length() == 1) return s;
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(b.count, a.count));
         HashMap<Character, Integer> hm = new HashMap<>();
-        int n = s.length();
-
-        for(int i = 0; i < n; i++){
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);    
+        for(int i = 0; i < s.length(); i++){
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
-        for(int i = 0; i < n; i++){
-            if(hm.containsKey(s.charAt(i))){
-                pq.offer(new Pair(s.charAt(i), hm.get(s.charAt(i))));
-                hm.remove(s.charAt(i));
-            }
+        for(Map.Entry<Character, Integer> ele : hm.entrySet()){
+            pq.offer(new Pair(ele.getKey(), ele.getValue()));
         }
-        
-        int index = 0;
-        while(!pq.isEmpty() && pq.size() >= 2){
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() > 1){
             Pair first = pq.poll();
             Pair second = pq.poll();
-
-            if(index == 0 || sb.charAt(index - 1) != first.letter){
-                sb.append(first.letter);
-                sb.append(second.letter);
+            
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == first.ch){
+                sb.append(second.ch);
+                sb.append(first.ch);
             }
             else{
-                sb.append(second.letter);
-                sb.append(first.letter);
+                sb.append(first.ch);
+                sb.append(second.ch);
             }
-
-            if(first.count != 1) pq.offer(new Pair(first.letter, first.count - 1));
-            if(second.count != 1) pq.offer(new Pair(second.letter, second.count - 1));
+            if(first.count != 1) pq.offer(new Pair(first.ch, first.count - 1));
+            if(second.count != 1) pq.offer(new Pair(second.ch, second.count - 1));
         }
+        
+        // System.out.println(sb.charAt(sb.length() - 1));
         if(pq.size() == 1){
-            if(sb.length() == 0 || sb.charAt(sb.length() - 1) != pq.peek().letter){
-                if(pq.peek().count > 1) return "";
-                sb.append(pq.peek().letter);
-            }
-            else return "";
+            if(pq.peek().count > 1 || sb.charAt(sb.length() - 1) == pq.peek().ch) return "";
+            else sb.append(pq.peek().ch);
         }
         return sb.toString();
     }
