f0721064cff298aa9acd4ec682e0f1beeb7603d1
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-09 10:17:00 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 55. Jump Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return true;
+
+        int goal = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+};
