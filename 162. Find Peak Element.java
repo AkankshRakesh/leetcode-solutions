de795af62f31b2f3987e65a82669806725d71c03
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-11 16:31:41 +0530

    Time complexity: O(logn), Space complexity: O(1)

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -1,15 +1,21 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        int left = 0, right = n - 1;
+        if(n == 1) return 0;
+        if(n == 2) return nums[0] > nums[1] ? 0 : 1;
+        int left = 0;
+        int right = nums.length - 1;
         while(left < right){
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
             if(mid > 0 && mid < n - 1){
-                if(nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) left = mid + 1;
+                if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+                else if(nums[mid] <= nums[mid + 1]) left = mid + 1;
                 else right = mid;
             }
-            else return nums[left] > nums[right] ? nums[left] > nums[mid] ? left : mid : nums[right] > nums[mid] ? right : mid;
+            else break;
         }
-        return left;
+        if(nums[n - 2] < nums[n - 1]) return n - 1;
+        if(nums[0] > nums[1]) return 0;
+        return -1;
     }
 }
