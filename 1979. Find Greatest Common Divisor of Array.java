c13db8988d70b9aef2dca8de589788df3e8a4841
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-15 15:26:08 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 1979. Find Greatest Common Divisor of Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+    public int findGCD(int[] nums) {
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+
+        return gcd(min, max);
+    }
+}
