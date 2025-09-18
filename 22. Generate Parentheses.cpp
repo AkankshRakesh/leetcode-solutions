14866bef2e568205fef846cfae6d49c1f2dcb414
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-18 13:06:38 +0530

    Time complexity: O(2^n), Space complexity: O(n)

--- 22. Generate Parentheses.cpp
+++ 22. Generate Parentheses.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    // Check if the string of parentheses is valid
     bool check(const string& s) {
         int value = 0;
         for (int i = s.length() - 1; i >= 0; --i) {
@@ -12,14 +11,13 @@ public:
         return true;
     }
 
-    // Recursive exploration function
     void exploration(vector<string>& res, string s, int leftPara, int rightPara) {
         if (leftPara == 0 && rightPara == 0) {
             if (check(s)) {
                 res.push_back(s);
             }
             return;
-        }
+        } 
         else if (leftPara == 0) {
             exploration(res, s + ")", leftPara, rightPara - 1);
         }
@@ -32,7 +30,6 @@ public:
         }
     }
 
-    // Main function to generate valid parenthesis combinations
     vector<string> generateParenthesis(int n) {
         vector<string> arr;
         exploration(arr, "(", n - 1, n);
