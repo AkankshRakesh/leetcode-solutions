dce305838516869968abd58dcaa7d06bdcea407c
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-20 12:17:21 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return true;
-        int goal = n - 1;
-        for(int i = n - 2; i >= 0; i--){
-            if(i + nums[i] >= goal) goal = i;
+        int ind = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(i + nums[i] >= ind) ind = i;
         }
-        if(goal == 0) return true;
+        System.out.println(ind);
+        if(ind == 0) return true;
         return false;
     }
 }
