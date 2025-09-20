d410e6eb22cc354b90b73d546406cc4d79b9ada9
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-20 15:33:14 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 55. Jump Game.cpp
+++ 55. Jump Game.cpp
@@ -14,3 +14,4 @@ public:
         return goal == 0;
     }
 };
+ 
--- 56. Merge Intervals.java
+++ 56. Merge Intervals.java
@@ -1,47 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
-            return Integer.compare(a[1], b[1]);
-        });
-
-        int n = intervals.length;
-        int low = intervals[0][0];
-        int high = intervals[0][1];
-        int index = 1;
-
-        for(int i = 1; i < n; i++){
-            if(intervals[i][0] <= high){
-                if(intervals[i][1] > high) high = intervals[i][1];
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        Stack<int[]> st = new Stack<>();
+        st.push(intervals[0]);
+        for(int i = 1; i < intervals.length; i++){
+            int[] last = st.peek();
+            // System.out.println(last[0]);
+            int[] curr = intervals[i];
+            if(last[1] >= curr[0]){
+                if(last[1] >= curr[1]) continue;
+                else{
+                    st.pop();
+                    st.push(new int[]{last[0], curr[1]});
+                }
             }
-            else{
-                index++;
-                low = intervals[i][0];
-                if(intervals[i][1] > high) high = intervals[i][1];
-            }
+            else st.push(curr);
         }
-        int[][] arr = new int[index][2];
-        index = 0;
-        low = intervals[0][0];
-        high = intervals[0][1];
-        for(int i = 1; i < n; i++){
-            if(intervals[i][0] <= high){
-                if(intervals[i][1] > high) high = intervals[i][1];
-            }
-            else{
-                int[] temp = new int[2];
-                temp[0] = low;
-                temp[1] = high;
-                arr[index] = temp;
-                index++;
-                low = intervals[i][0];
-                if(intervals[i][1] > high) high = intervals[i][1];
-            }
+        
+        int[][] ans = new int[st.size()][2];
+        int ind = 0;
+        while(!st.isEmpty()){
+            // System.out.println(st.peek()[0]);
+            ans[ind++] = st.pop();
         }
-        int[] temp = new int[2];
-        temp[0] = low;
-        temp[1] = high;
-        arr[index] = temp;
-        return arr;
+
+        return ans;
     }
 }
