96a7d2dfb3220ac3ab67a7dd46c3418d440e6d9c
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-20 15:59:21 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

---
+++ 57. Insert Interval.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        Stack<int[]> st = new Stack<>();
+        st.push(intervals[0]);
+        for(int i = 1; i < intervals.length; i++){
+            int[] last = st.peek();
+            // System.out.println(last[0]);
+            int[] curr = intervals[i];
+            if(last[1] >= curr[0]){
+                if(last[1] >= curr[1]) continue;
+                else{
+                    st.pop();
+                    st.push(new int[]{last[0], curr[1]});
+                }
+            }
+            else st.push(curr);
+        }
+        
+        int[][] ans = new int[st.size()][2];
+        int ind = 0;
+        while(!st.isEmpty()){
+            // System.out.println(st.peek()[0]);
+            ans[ind++] = st.pop();
+        }
+
+        return ans;
+    }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] preAns = new int[intervals.length + 1][2];
+        preAns[0] = newInterval;
+        for(int i = 0; i < intervals.length; i++){
+            preAns[i + 1] = intervals[i];
+        }
+
+        int[][] ans = merge(preAns);
+        Arrays.sort(ans, (a, b) -> Integer.compare(a[0], b[0]));
+        return ans;
+    }
+}
