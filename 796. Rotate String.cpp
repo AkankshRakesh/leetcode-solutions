78ae8e301888ddde1d64691db36d90fbdd81c1e8
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-23 15:43:45 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 796. Rotate String.cpp
+++ 796. Rotate String.cpp
@@ -1,10 +1,48 @@
-#include <iostream>
-using namespace std;
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()) return false;
+    vector<int> lps(const string& s) {
+        vector<int> lps(s.length());
+        int i = 1;
+        int len = 0;
+        while (i < (int)s.length()) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
 
-        return (s + s).find(goal) < s.length();
+    bool search(const string& txt, const string& pat) {
+        vector<int> lp = lps(pat);
+        int i = 0, j = 0;
+        while (i < (int)txt.length()) {
+            if (txt[i] == pat[j]) {
+                i++;
+                j++;
+                if (j == (int)pat.length()) return true;
+            } else {
+                if (j != 0) {
+                    j = lp[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        s += s;
+        return search(s, goal);
     }
 };
