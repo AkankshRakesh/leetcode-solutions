ea47ae8459d2266fbbb471edb0b138d19ac40afe
AkankshRakesh <akankshrakesh@gmail.com>
2025-09-24 16:50:43 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 134. Gas Station.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int tot = 0;
+        for (int i = 0; i < n; i++) {
+            tot += gas[i] - cost[i];
+        }
+        if (tot < 0) return -1;
+
+        tot = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = gas[i] - cost[i];
+            tot += diff;
+            if (tot < 0) {
+                ans = i + 1;
+                tot = 0;
+            }
+        }
+
+        return ans;
+    }
+};
