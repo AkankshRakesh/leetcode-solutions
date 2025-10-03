d75554cee3562443f5bcd01dad3cae2015ae597c
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-03 11:59:15 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 435. Non-overlapping Intervals.java
+++ 435. Non-overlapping Intervals.java
@@ -1,22 +1,24 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals,(a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
-        int prevInt = intervals[0][1];
-        int ans = 0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int count = 0;
+        int x1 = intervals[0][0];
+        int y1 = intervals[0][1];
+        // System.out.println(y1);
         for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][0] >= prevInt){
-                prevInt = intervals[i][1];
-                continue;
+            int x2 = intervals[i][0];
+            int y2 = intervals[i][1];
+            if(y1 <= x2){
+                x1 = x2;
+                y1 = y2;
             }
-            else if(intervals[i][0] < prevInt){
-                if(intervals[i][1] < prevInt){
-                    ans++;
-                    prevInt = intervals[i][1];
-                }
-                else ans++;
+            else if(y1 > y2){
+                y1 = y2;
+                count++;
             }
+            else count++;
         }
 
-        return ans;
+        return count;
     }
 }
