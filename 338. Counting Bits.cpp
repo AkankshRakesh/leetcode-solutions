f6d0aa845abc7b83d41b6175b5af0a4608bacf91
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-15 12:27:29 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-       std::vector<int> dp(n + 1, 0);
-        int sub = 1;
-
-        for (int i = 1; i <= n; i++) {
-            if (sub * 2 == i) {
-                sub = i;
+       int[] ans = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            int count = 0;
+            int num = i;
+            // System.out.println(num);
+            for(int j = 0; j < 32; j++){
+                count += (num & 1);
+                num = num >> 1;
             }
-
-            dp[i] = dp[i - sub] + 1;
+            ans[i] = count;
         }
 
-        return dp;        
+        return ans;      
     }
 };
