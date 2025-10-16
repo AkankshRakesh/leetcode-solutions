fbcb191e531a7d6ffc8e96feefa808a087423cf2
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-16 09:41:05 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 2598. Smallest Missing Non-negative Integer After Operations.cpp
+++ 2598. Smallest Missing Non-negative Integer After Operations.cpp
@@ -1,20 +1,23 @@
 class Solution {
-    public int findSmallestInteger(int[] nums, int value) {
-        int[] remArr = new int[value];
-        for(int num : nums){
-            if(num < 0){
-                int rem = -num % value;
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        vector<int> remArr(value, 0);
+
+        for (int num : nums) {
+            if (num < 0) {
+                int rem = (-num) % value;
                 remArr[(value - rem) % value]++;
+            } else {
+                remArr[num % value]++;
             }
-            else remArr[num % value]++;
         }
 
         int ans = 0;
-        while(remArr[ans % value] > 0){
+        while (remArr[ans % value] > 0) {
             remArr[ans % value]--;
             ans++;
         }
 
         return ans;
     }
-}
+};
