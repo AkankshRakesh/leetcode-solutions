bce029c033735f0b0e2de60a437be74139bb84d7
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-17 11:09:58 +0530

    Time complexity: O(n*m), Space complexity: O(n*m)

--- 417. Pacific Atlantic Water Flow.cpp
+++ 417. Pacific Atlantic Water Flow.cpp
@@ -1,41 +1,55 @@
 class Solution {
-    int n, m;
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+public:
+    void dfs(vector<vector<int>>& heights, int i, int j, int n, int m, vector<vector<bool>>& visited, int past) {
+        if (i >= n || j >= m || i < 0 || j < 0) return;
+        if (heights[i][j] < past || visited[i][j]) return;
 
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        n = heights.length;
-        m = heights[0].length;
+        visited[i][j] = true;
+        dfs(heights, i - 1, j, n, m, visited, heights[i][j]);
+        dfs(heights, i + 1, j, n, m, visited, heights[i][j]);
+        dfs(heights, i, j + 1, n, m, visited, heights[i][j]);
+        dfs(heights, i, j - 1, n, m, visited, heights[i][j]);
+    }
 
-        boolean[][] pacific = new boolean[n][m];
-        boolean[][] atlantic = new boolean[n][m];
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+
+        vector<vector<int>> ans;
+        vector<vector<bool>> pacific(n, vector<bool>(m, false));
+        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
-            dfs(heights, pacific, i, 0, Integer.MIN_VALUE);   
-            dfs(heights, atlantic, i, m - 1, Integer.MIN_VALUE); 
+            pacific[i][0] = true;
+            atlantic[i][m - 1] = true;
         }
         for (int j = 0; j < m; j++) {
-            dfs(heights, pacific, 0, j, Integer.MIN_VALUE);     
-            dfs(heights, atlantic, n - 1, j, Integer.MIN_VALUE); 
+            pacific[0][j] = true;
+            atlantic[n - 1][j] = true;
         }
 
-        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            dfs(heights, i, 1, n, m, pacific, heights[i][0]);
+        }
+        for (int j = 0; j < m; j++) {
+            dfs(heights, 1, j, n, m, pacific, heights[0][j]);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            dfs(heights, i, m - 2, n, m, atlantic, heights[i][m - 1]);
+        }
+        for (int j = m - 1; j >= 0; j--) {
+            dfs(heights, n - 2, j, n, m, atlantic, heights[n - 1][j]);
+        }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (pacific[i][j] && atlantic[i][j]) {
-                    ans.add(Arrays.asList(i, j));
+                    ans.push_back({i, j});
                 }
             }
         }
+
         return ans;
     }
-
-    private void dfs(int[][] heights, boolean[][] visited, int i, int j, int prevHeight) {
-        if (i < 0 || j < 0 || i >= n || j >= m) return;
-        if (visited[i][j] || heights[i][j] < prevHeight) return;
-
-        visited[i][j] = true;
-        for (int[] d : dirs) {
-            dfs(heights, visited, i + d[0], j + d[1], heights[i][j]);
-        }
-    }
-}
+};
