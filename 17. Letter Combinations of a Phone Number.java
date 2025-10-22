6db30e42d399fe8a29145f8fb735e7b85ac960a1
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-22 11:17:01 +0530

    Time complexity: O(4^n * n), Space complexity: O(4^n * n)

--- 17. Letter Combinations of a Phone Number.java
+++ 17. Letter Combinations of a Phone Number.java
@@ -1,74 +1,31 @@
 class Solution {
-    public void backtrack(String digits, int index, int n, ArrayList<String> arr, String curr, HashMap<Character, ArrayList<String>> hm){
-        if(index == n){
-            if(curr != "") arr.add(curr);
+    public char[] numToLetter(int num){
+        if(num == 2) return new char[]{'a', 'b', 'c'};
+        else if(num == 3) return new char[]{'d', 'e', 'f'};
+        else if(num == 4) return new char[]{'g', 'h', 'i'};
+        else if(num == 5) return new char[]{'j', 'k', 'l'};
+        else if(num == 6) return new char[]{'m', 'n', 'o'};
+        else if(num == 7) return new char[]{'p', 'q', 'r', 's'};
+        else if(num == 8) return new char[]{'t', 'u', 'v'};
+        else return new char[]{'w', 'x', 'y', 'z'};
+    }
+    public void backtrack(List<String> ans, StringBuilder curr, String digits, int index){
+        if(index >= digits.length()){
+            ans.add(curr.toString());
             return;
         }
 
-        ArrayList<String> list = hm.get(digits.charAt(index));
-        for(int i = 0; i < list.size(); i++){
-            backtrack(digits, index + 1, n, arr, curr + list.get(i), hm);
+        int digit = digits.charAt(index) - '0';
+        for(char ch : numToLetter(digit)){
+            curr.append(ch);
+            backtrack(ans, curr, digits, index + 1);
+            curr.deleteCharAt(curr.length() - 1);
         }
     }
     public List<String> letterCombinations(String digits) {
-        ArrayList<String> arr = new ArrayList<String>();
-        HashMap<Character, ArrayList<String>> hm = new HashMap<Character, ArrayList<String>>();
-        for(int i = 2; i < 10; i++){
-            ArrayList<String> temp = new ArrayList<String>();
-            switch(i){
-                case 2:
-                    temp.add("a");
-                    temp.add("b");
-                    temp.add("c");
-                    hm.put('2', temp);
-                    break;
-                case 3:
-                    temp.add("d");
-                    temp.add("e");
-                    temp.add("f");
-                    hm.put('3', temp);
-                    break;
-                case 4:
-                    temp.add("g");
-                    temp.add("h");
-                    temp.add("i");
-                    hm.put('4', temp);
-                    break;
-                case 5:
-                    temp.add("j");
-                    temp.add("k");
-                    temp.add("l");
-                    hm.put('5', temp);
-                    break;
-                case 6:
-                    temp.add("m");
-                    temp.add("n");
-                    temp.add("o");
-                    hm.put('6', temp);
-                    break;
-                case 7:
-                    temp.add("p");
-                    temp.add("q");
-                    temp.add("r");
-                    temp.add("s");
-                    hm.put('7', temp);
-                    break;
-                case 8:
-                    temp.add("t");
-                    temp.add("u");
-                    temp.add("v");
-                    hm.put('8', temp);
-                    break;
-                case 9:
-                    temp.add("w");
-                    temp.add("x");
-                    temp.add("y");
-                    temp.add("z");
-                    hm.put('9', temp);
-                    break;
-            }
-        }
-        backtrack(digits, 0, digits.length(), arr, "", hm);
-        return arr;
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), digits, 0);
+        
+        return ans;
     }
 }
