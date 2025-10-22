bcc56e0aa142e9dc32ba6740deb96e8bcd35c2ee
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-22 12:32:30 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -1,15 +1,23 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
+public:
+    int findDuplicate(vector<int>& nums) {
         int ans = 0;
-        for(int bit = 0; bit < 32; bit++){
+
+        for (int bit = 0; bit < 32; bit++) {
             int mask = 1 << bit;
             int x = 0, y = 0;
-            for(int num: nums) if((num & mask) == mask) x++;
-            for(int i = 1; i < nums.length; i++) if((i & mask) == mask) y++;
 
-            if(x > y) ans |= mask;
+            for (int num : nums) {
+                if ((num & mask) == mask) x++;
+            }
+
+            for (int i = 1; i < (int)nums.size(); i++) {
+                if ((i & mask) == mask) y++;
+            }
+
+            if (x > y) ans |= mask;
         }
 
         return ans;
     }
-}
+};
