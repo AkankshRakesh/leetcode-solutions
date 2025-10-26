3ddb466704b6055977867661f1b5c0dfbbd93425
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-26 22:43:46 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 56. Merge Intervals.java
+++ 56. Merge Intervals.java
@@ -1,30 +1,34 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        Stack<int[]> st = new Stack<>();
-        st.push(intervals[0]);
-        for(int i = 1; i < intervals.length; i++){
-            int[] last = st.peek();
-            // System.out.println(last[0]);
-            int[] curr = intervals[i];
-            if(last[1] >= curr[0]){
-                if(last[1] >= curr[1]) continue;
-                else{
-                    st.pop();
-                    st.push(new int[]{last[0], curr[1]});
-                }
-            }
-            else st.push(curr);
+    class Pair {
+        int start;
+        int end;
+        public Pair(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
-        
-        int[][] ans = new int[st.size()][2];
-        int ind = 0;
-        while(!st.isEmpty()){
-            // System.out.println(st.peek()[0]);
-            ans[ind++] = st.pop();   
+    }
+
+    public int[][] merge(int[][] invs) {
+        Arrays.sort(invs, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+        List<Pair> merged = new ArrayList<>();
+        merged.add(new Pair(invs[0][0], invs[0][1]));
+
+        for (int i = 1; i < invs.length; i++) {
+            Pair last = merged.get(merged.size() - 1);
+            if (last.end >= invs[i][0]) {
+                last.end = Math.max(last.end, invs[i][1]);
+            } else {
+                merged.add(new Pair(invs[i][0], invs[i][1]));
+            }
         }
 
-        return ans; 
+        int[][] ans = new int[merged.size()][2];
+        for (int i = 0; i < merged.size(); i++) {
+            ans[i][0] = merged.get(i).start;
+            ans[i][1] = merged.get(i).end;
+        }
+
+        return ans;
     }
 }
