e2951b58b1130061004556c4c6a21038c7b64093
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-26 21:51:41 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -1,21 +1,16 @@
-#include <queue>
-#include <vector>
-#include <climits>
-using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min = INT_MAX, max = INT_MIN;
-        priority_queue<int> pq;
-        for(int i = 0; i < prices.size(); i++){
-            if(min > prices[i]){
-                if(max != INT_MIN && min != INT_MAX) pq.push(max - min);
-                min = prices[i];
-                max = prices[i];
+        int minPrice = INT_MAX;
+        int profit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
             }
-            if(max < prices[i]) max = prices[i];
+            profit = max(profit, price - minPrice);
         }
-        if(max != INT_MIN && min != INT_MAX) pq.push(max - min);
-        return pq.top();
+
+        return profit;
     }
 };
