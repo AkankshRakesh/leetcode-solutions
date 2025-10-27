2778025be3884612c8f373865f87071d16a0308b
AkankshRakesh <akankshrakesh@gmail.com>
2025-10-27 10:43:07 +0530

    Time complexity: O(n*n), Space complexity: O(1)

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -3,21 +3,17 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        // Transpose using XOR
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                matrix[i][j] ^= matrix[j][i];
-                matrix[j][i] ^= matrix[i][j];
-                matrix[i][j] ^= matrix[j][i];
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-        }
 
-        // Reverse each row using XOR
-        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n / 2; j++) {
-                matrix[i][j] ^= matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] ^= matrix[i][j];
-                matrix[i][j] ^= matrix[i][n - 1 - j];
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
             }
         }
     }
