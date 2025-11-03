1ca5a161c28244c1861aca7925d5b8e0ea1aa9d1
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-03 19:30:16 +0530

    Time complexity: O(n(rootn)), Space complexity: O(1)

---
+++ Twisted Prime Number.cpp
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public boolean isPrime(int n){
+        if(n <= 1) return false;
+        for(int i = 2; i * i <= n; i++) if(n % i == 0) return false;
+        
+        return true;
+    }
+    int isTwistedPrime(int N) {
+        if(!isPrime(N)) return 0;
+        StringBuilder sb = new StringBuilder(Integer.toString(N));
+        sb.reverse();
+        Integer revN = Integer.parseInt(sb.toString());
+        if(!isPrime(revN)) return 0;
+        
+        return 1;
+    }
+}
