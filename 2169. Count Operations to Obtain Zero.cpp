2e317b7aa33e020a254c468e4ed7bb2f85beb9ab
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-09 08:32:53 +0530

    Time complexity: O(min(n1, n2)), Space complexity: O(1)

--- 2169. Count Operations to Obtain Zero.cpp
+++ 2169. Count Operations to Obtain Zero.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    public int countOperations(int num1, int num2) {
+public:
+    int countOperations(int num1, int num2) {
         int ans = 0;
         while(num1 != 0 && num2 != 0){
             ans++;
@@ -9,4 +10,4 @@ class Solution {
 
         return ans;
     }
-}
+};
