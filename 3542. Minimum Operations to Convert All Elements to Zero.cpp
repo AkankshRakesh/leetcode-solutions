832e4749f47376feb4aca2bf33b93eb932f4e0bf
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-10 10:35:16 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 3542. Minimum Operations to Convert All Elements to Zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        stack<int> s;
+        int res = 0;
+        for (int a : nums) {
+            while (!s.empty() && s.top() > a)
+                s.pop();
+            if (a == 0) continue;
+            if (s.empty() || s.top() < a) {
+                res++;
+                s.push(a);
+            }
+        }
+        return res;
+    }
+};
