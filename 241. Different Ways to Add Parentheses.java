bd6d80e1c02600a8bbd5b954b07584d1a6a2f521
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-16 12:59:32 +0530

    Time complexity: O(2^n), Space complexity: O(2^n)

--- 241. Different Ways to Add Parentheses.java
+++ 241. Different Ways to Add Parentheses.java
@@ -1,49 +1,43 @@
 class Solution {
-    public void merge(List<Integer> res, List<Integer> x, List<Integer> y, char opt){
-        if(opt == '+'){
-            for(int ele1 : x){
-                for(int ele2 : y){
-                    res.add(ele1 + ele2);
-                }
-            }
-        }
-        else if(opt == '-'){
-            for(int ele1 : x){
-                for(int ele2 : y){
-                    res.add(ele1 - ele2);
-                }
-            }
-        }
-        else{
-            for(int ele1 : x){
-                for(int ele2 : y){
-                    res.add(ele1 * ele2);
-                }
-            }
-        }
-    }
-    public List<Integer> divide(StringBuilder exp){
+    public List<Integer> compute(StringBuilder exp){
         List<Integer> res = new ArrayList<>();
-        boolean optFound = false;
+        boolean isNum = true;
         for(int i = 0; i < exp.length(); i++){
-            if(exp.charAt(i) == '+' || exp.charAt(i) == '-' || exp.charAt(i) == '*'){
-                optFound = true;
-                StringBuilder part1 = new StringBuilder();
-                StringBuilder part2 = new StringBuilder();
-                for(int j = 0; j < i; j++) part1.append(exp.charAt(j));
-                for(int j = i + 1; j < exp.length(); j++) part2.append(exp.charAt(j));
-                List<Integer> x = divide(part1);
-                List<Integer> y = divide(part2);
-                merge(res, x, y, exp.charAt(i));
+            if(exp.charAt(i) < '0' || exp.charAt(i) > '9'){
+                isNum = false;
+                List<Integer> left = compute(new StringBuilder(exp.substring(0, i)));
+                List<Integer> right = compute(new StringBuilder(exp.substring(i + 1)));
+                switch(exp.charAt(i)){
+                    case '+':
+                        for(int leftRes : left){
+                            for(int rightRes : right){
+                                res.add(leftRes + rightRes);
+                            }
+                        }
+                        break;
+                    case '-':
+                        for(int leftRes : left){
+                            for(int rightRes : right){
+                                res.add(leftRes - rightRes);
+                            }
+                        }
+                        break;
+                    case '*':
+                        for(int leftRes : left){
+                            for(int rightRes : right){
+                                res.add(leftRes * rightRes);
+                            }
+                        }
+                        break;
+                }
             }
-        }
-        if(!optFound){
-            res.add(Integer.parseInt(exp.toString()));
         }
 
+        if(isNum) res.add(Integer.parseInt(exp.toString()));
         return res;
     }
     public List<Integer> diffWaysToCompute(String expression) {
-        return divide(new StringBuilder(expression));
+        List<Integer> ans = new ArrayList<>();
+        return compute(new StringBuilder(expression));
     }
 }
