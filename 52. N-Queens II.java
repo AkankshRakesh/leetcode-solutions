1638ff9952d3f8c05231aa0171039693d3763cf2
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-17 19:15:34 +0530

    Time complexity: O(2^n), Space complexity: O(n)

--- 52. N-Queens II.java
+++ 52. N-Queens II.java
@@ -1,33 +1,36 @@
 class Solution {
-    int ans = 0;
-    public boolean check(boolean[][] queens, int x, int y){
-        int n = queens.length;
-        for(int j = y; j >= 0; j--) if(queens[x][j]) return false;
-        for(int i = x; i >= 0; i--) if(queens[i][y]) return false;
+    public boolean canPlace(boolean[][] queens, int row, int col){
+        if(row >= queens.length) return false;
+        for(int i = row; i >= 0; i--) if(queens[i][col]) return false;
 
-        for(int i = x, j = y; i >= 0 && j >= 0; i--, j--) if(queens[i][j]) return false;
-        for(int i = x, j = y; i >= 0 && j < n; i--, j++) if(queens[i][j]) return false;
-
-        return true;
-    }
-    public void backtrack(boolean[][] queens, int row){
-        int n = queens.length;
-        if(row >= n){
-            ans++;
-            return;
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--){
+            if(queens[i][j]) return false;
         }
 
+        for(int i = row, j = col; i >= 0 && j < queens.length; i--, j++){
+            if(queens[i][j]) return false;
+        }
+        
+        return true;
+    }
+
+    public int backtrack(boolean[][] queens, int row, int n, int currQueens){
+        if(row >= n) return 1;
+        // System.out.println(row);
+
+        int res = 0;
         for(int col = 0; col < n; col++){
-            if(check(queens, row, col)){
+            if(canPlace(queens, row, col)){
                 queens[row][col] = true;
-                backtrack(queens, row + 1);
+                res += backtrack(queens, row + 1, n, currQueens + 1);
                 queens[row][col] = false;
             }
         }
+
+        return res;
     }
     public int totalNQueens(int n) {
         boolean[][] queens = new boolean[n][n];
-        backtrack(queens, 0);
-        return ans;
+        return backtrack(queens, 0, n, 0);
     }
 }
