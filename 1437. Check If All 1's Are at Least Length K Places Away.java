3c9375d042d51f68524a93637f7255ec863b9c3b
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-17 09:53:23 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 1437. Check If All 1's Are at Least Length K Places Away.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int last = -1;
+        int ind = 0;
+        for(int num: nums){
+            if(num == 1){
+                if(last == -1) last = ind;
+                else{
+                    if(ind - last - 1 < k) return false;
+                    last = ind;
+                }
+            }
+            ind++;
+        }
+
+        return true;
+    }
+}
