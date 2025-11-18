d027a94d5651de5877b61626b175e56f5a56e6f9
AkankshRakesh <akankshrakesh@gmail.com>
2025-11-18 12:15:49 +0530

    Time complexity: O(4^n), Space complexity: O(n)

---
+++ 980. Unique Paths III.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int backtrack(int[][] grid, boolean[][] visited, int x, int y, int n, int m){
+        if(x >= n || y >= m || x < 0 || y < 0 || visited[x][y] || grid[x][y] == -1) return 0;
+        if(grid[x][y] == 2){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(grid[i][j] == 0 && !visited[i][j]) return 0; 
+                }
+            }
+
+            return 1;
+        }
+
+        visited[x][y] = true;
+        int res = 0;
+        res += backtrack(grid, visited, x + 1, y, n, m);
+        res += backtrack(grid, visited, x - 1, y, n, m);
+        res += backtrack(grid, visited, x, y + 1, n, m);
+        res += backtrack(grid, visited, x, y - 1, n, m);
+        visited[x][y] = false;
+
+        return res;
+    }
+    public int uniquePathsIII(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+
+        int stIndX = -1;
+        int stIndY = -1;
+        for(int i = 0; i < n; i++){
+            boolean found = false;
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    stIndX = i;
+                    stIndY = j;
+                    found = true;
+                    break;
+                }
+            }
+            if(found) break;
+        }
+
+        return backtrack(grid, visited, stIndX, stIndY, n, m);
+    }
+}
