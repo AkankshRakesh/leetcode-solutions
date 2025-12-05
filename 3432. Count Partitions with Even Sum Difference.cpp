94cdcbb985954f7f8d25dbf4fe21cee218c86e46
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-05 10:10:00 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 3432. Count Partitions with Even Sum Difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 == 0) return nums.size() - 1;
+        else return 0;
+    }
+};
