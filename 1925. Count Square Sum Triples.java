000183eaa0f5055f49197e709680631c5de1321d
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-08 17:41:46 +0530

    Time complexity: O(n^2), Space complexity: O(1)

---
+++ 1925. Count Square Sum Triples.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countTriples(int n) {
+        int ans = 0;
+        for(int i = 2; i <= n; i++){
+            int target = i * i;
+            for(int a = 1; a < i; a++){
+                int bSquare = target - (a * a);
+                if(bSquare <= 0) continue;
+                int b = (int)Math.sqrt(bSquare);
+                
+                if(b * b == bSquare && a > b) ans += 2;
+            }
+        }
+        return ans;
+    }
+}
