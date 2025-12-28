7bf5137d92937df2407afd83411bdfeda6c9ae8c
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-28 21:49:39 +0530

    Time complexity: O(nlogn), Space complexity: O(1)

--- 532. K-diff Pairs in an Array.cpp
+++ 532. K-diff Pairs in an Array.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        unordered_map<int, int> hm;
-        for (int num : nums) hm[num]++;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int n = nums.size();
+        unordered_set<int> hs;
 
-        int count = 0;
-        for (auto &ele : hm) {
-            int num = ele.first;
-            if (k == 0) {
-                if (hm[num] > 1) count++;
-            } else {
-                if (hm.find(num + k) != hm.end()) count++;
+        for (int i = 0; i < n; i++) {
+            int a = nums[i];
+            if (hs.count(a)) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == k + a) {
+                    hs.insert(a);
+                    ans++;
+                    break;
+                }
+                else if (nums[mid] > k + a) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
             }
         }
 
-        return count;
+        return ans;
     }
 };
