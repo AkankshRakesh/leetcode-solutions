c58d5986b000e03619f99a4377672b458039d69a
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-28 13:54:58 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 581. Shortest Unsorted Continuous Subarray.cpp
+++ 581. Shortest Unsorted Continuous Subarray.cpp
@@ -1,41 +1,47 @@
 class Solution {
-    public int findUnsortedSubarray(int[] nums) {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
         int leftInd = -1;
         int rightInd = -1;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] < nums[i - 1]){
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
                 leftInd = i - 1;
                 break;
             }
         }
-        if(leftInd == -1) return 0;
-        for(int i = nums.length - 2; i >= leftInd; i--){
-            if(nums[i] > nums[i + 1]){
+
+        if (leftInd == -1) return 0;
+
+        for (int i = nums.size() - 2; i >= leftInd; i--) {
+            if (nums[i] > nums[i + 1]) {
                 rightInd = i + 1;
                 break;
             }
         }
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i = leftInd; i <= rightInd; i++){
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
+
+        int minVal = INT_MAX;
+        int maxVal = INT_MIN;
+
+        for (int i = leftInd; i <= rightInd; i++) {
+            minVal = min(minVal, nums[i]);
+            maxVal = max(maxVal, nums[i]);
         }
 
-
-        for(int i = leftInd - 1; i >= 0; i--){
-            if(nums[i] > min){
+        for (int i = leftInd - 1; i >= 0; i--) {
+            if (nums[i] > minVal) {
                 leftInd = i;
-                max = Math.max(max, nums[i]);
+                maxVal = max(maxVal, nums[i]);
             }
         }
-        for(int i = rightInd + 1; i < nums.length; i++){
-            if(nums[i] < max){
+
+        for (int i = rightInd + 1; i < nums.size(); i++) {
+            if (nums[i] < maxVal) {
                 rightInd = i;
-                min = Math.min(min, nums[i]);
+                minVal = min(minVal, nums[i]);
             }
         }
 
         return rightInd - leftInd + 1;
     }
-}
+};
