ef59a90385c6079650508c7a55d59296ec988235
AkankshRakesh <akankshrakesh@gmail.com>
2025-12-28 20:04:37 +0530

    Time complexity: O(nlogn), Space complexity: O(1)

--- 532. K-diff Pairs in an Array.java
+++ 532. K-diff Pairs in an Array.java
@@ -1,18 +1,27 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0;
         int n = nums.length;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int num: nums) hm.put(num, hm.getOrDefault(num, 0) + 1);
-
-        int count = 0;
-        for(Map.Entry<Integer, Integer> ele : hm.entrySet()){
-            int num = ele.getKey();
-            if(k == 0){
-                if(hm.get(num) > 1) count++;
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int i = 0; i < n; i++){
+            int a = nums[i];
+            if(hs.contains(a)) continue;
+            int left = i + 1;
+            int right = n - 1;
+            while(left <= right){
+                int mid = left + (right - left) / 2;
+                if(nums[mid] == k + a){
+                    // hs.add(nums[mid]);
+                    hs.add(a);
+                    ans++;
+                    break;
+                }
+                else if(nums[mid] > k + a) right = mid - 1;
+                else left = mid + 1;
             }
-            else if(hm.containsKey(k + num)) count++;
         }
 
-        return count;
+        return ans;
     }
 }
