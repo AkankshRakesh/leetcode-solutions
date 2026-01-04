517eedb5a28091c7361dc943fabf43bc1c9ccb57
AkankshRakesh <akankshrakesh@gmail.com>
2026-01-04 11:21:53 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

--- 1390. Four Divisors.cpp
+++ 1390. Four Divisors.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-        int ans = 0;
-        for(int num : nums){
-            int currSum = 0;
-            bool found = false;
-            for(int i = 2; i * i <= num; i++){
-                if(i * i == num){
-                    found = false;
-                    break;
-                }
-                if(num % i == 0){
-                    if(found){
-                        found = false;
-                        break;
-                    }
-                    found = true;
-                    currSum += (i + (num / i));
-                }
+        int maxNum = -1;
+        for (int num : nums)
+            maxNum = max(maxNum, num);
+
+        vector<int> divCount(maxNum + 1, 0);
+        vector<vector<int>> divisors(maxNum + 1, vector<int>(2, 0));
+
+        for (int i = 2; i <= maxNum; i++) {
+            for (int j = i + i; j <= maxNum; j += i) {
+                divCount[j]++;
+                if (divCount[j] == 1)
+                    divisors[j][0] = i;
+                else
+                    divisors[j][1] = i;
             }
-            if(found) ans += (currSum + 1 + num);
         }
+
+        int ans = 0;
+        for (int num : nums) {
+            if (divCount[num] == 2) {
+                ans += (1 + num + divisors[num][0] + divisors[num][1]);
+            }
+        }
+
         return ans;
     }
 };
