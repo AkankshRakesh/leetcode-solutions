8b05dceb013a1b410d7731dcdf44c3c91453b087
AkankshRakesh <akankshrakesh@gmail.com>
2026-01-22 18:55:58 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 322. Coin Change.java
+++ 322. Coin Change.java
@@ -1,17 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);  
         dp[0] = 0;
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        for(int i = 1; i <= amount; i++){
+            dp[i] = Integer.MAX_VALUE;
+            for(int coin : coins){
+                if(i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+            }    
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
