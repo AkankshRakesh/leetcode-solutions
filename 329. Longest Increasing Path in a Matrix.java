687c55cdb6b50022ab173a9966836a4570b922a0
AkankshRakesh <akankshrakesh@gmail.com>
2026-01-28 19:38:59 +0530

    Time complexity: O(n*m*log(n)), Space complexity: O(n*m)

--- 329. Longest Increasing Path in a Matrix.java
+++ 329. Longest Increasing Path in a Matrix.java
@@ -1,36 +1,19 @@
 class Solution {
-    public int trav(int[][] matrix, int[][] memo, int lastEle, int i, int j, int n, int m){
-        if(i < 0 || j < 0 || i >= n || j >= m) return 0;
+    public int dfs(int[][] matrix, int i, int j, int[][] dp, boolean[][] visited, int prev){
+        if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] <= prev) return 0;
+        if(dp[i][j] != 0) return dp[i][j];
 
-        if(lastEle >= matrix[i][j]) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-
-        int found = 1 + Math.max(trav(matrix,memo, matrix[i][j], i + 1, j, n, m), Math.max(trav(matrix,memo, matrix[i][j], i - 1, j, n, m), Math.max(trav(matrix, memo, matrix[i][j], i, j + 1, n, m), trav(matrix,memo, matrix[i][j], i, j - 1, n, m))));
-
-        memo[i][j] = found;
-        return found;
+        return dp[i][j] = 1 + Math.max(dfs(matrix, i + 1, j, dp, visited, matrix[i][j]), Math.max(dfs(matrix, i - 1, j, dp, visited, matrix[i][j]), Math.max(dfs(matrix, i, j + 1, dp, visited, matrix[i][j]), dfs(matrix, i, j - 1, dp, visited, matrix[i][j]))));
     }
     public int longestIncreasingPath(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-
+        int[][] dp = new int[matrix.length][matrix[0].length];
         int max = 1;
-        int[][] memo = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++) memo[i][j] = -1;
-        }
 
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(memo[i][j] != -1){
-                    max = Math.max(max, memo[i][j]);
-                    continue;
-                }
-
-                int found = 1 + Math.max(trav(matrix,memo, matrix[i][j], i + 1, j, n, m), Math.max(trav(matrix,memo, matrix[i][j], i - 1, j, n, m), Math.max(trav(matrix, memo, matrix[i][j], i, j + 1, n, m), trav(matrix,memo, matrix[i][j], i, j - 1, n, m))));
-                memo[i][j] = found;
-                max = Math.max(max, found);
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+                dfs(matrix, i, j, dp, visited, Integer.MIN_VALUE);
+                max = Math.max(max, dp[i][j]);
             }
         }
 
