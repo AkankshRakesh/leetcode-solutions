c2d9b51e79d5a4b61f23e25fa82dba6ce8f37aca
AkankshRakesh <akankshrakesh@gmail.com>
2026-01-28 19:39:29 +0530

    Time complexity: O(n*m*log(n)), Space complexity: O(n*m)

--- 778. Swim in Rising Water.java
+++ 778. Swim in Rising Water.java
@@ -1,34 +1,25 @@
 class Solution {
-    class Pair{
-        int i;
-        int j;
-        public Pair(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
+    public boolean dfs(int[][] grid, int i, int j, int cost, boolean[][] visited){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] || grid[i][j] > cost) return false;
+        if(i == grid.length - 1 && j == grid[0].length - 1) return true;
+
+        visited[i][j] = true;
+        return dfs(grid, i + 1, j, cost, visited) | dfs(grid, i - 1, j, cost, visited) | dfs(grid, i, j + 1, cost, visited) | dfs(grid, i, j - 1, cost, visited);
     }
     public int swimInWater(int[][] grid) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(grid[a.i][a.j], grid[b.i][b.j]));
-        int n = grid.length;
-        int ans = 0;
-        pq.offer(new Pair(0, 0));
-        boolean[][] visited = new boolean[n][n];
-        
-        while(!pq.isEmpty()){
-            Pair top = pq.poll();
-            int i = top.i;
-            int j = top.j;
-            visited[i][j] = true;
-            ans = Math.max(ans, grid[i][j]);
+        int left = 0, right = grid.length * grid.length;
+        int ans = Integer.MAX_VALUE;
 
-            if(i - 1 >= 0 && !visited[i - 1][j]) pq.offer(new Pair(i - 1, j));   
-            if(i + 1 < n && !visited[i + 1][j]) pq.offer(new Pair(i + 1, j));   
-            if(j - 1 >= 0 && !visited[i][j - 1]) pq.offer(new Pair(i, j - 1));   
-            if(j + 1 < n && !visited[i][j + 1]) pq.offer(new Pair(i, j + 1));   
-            
-            if(i == n - 1 && j == n - 1) break;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            boolean[][] visited = new boolean[grid.length][grid[0].length];
+            if(dfs(grid, 0, 0, mid, visited)){
+                ans = mid;
+                right = mid;
+            }
+            else left = mid + 1;
         }
 
         return ans;
     }
-} 
+}
