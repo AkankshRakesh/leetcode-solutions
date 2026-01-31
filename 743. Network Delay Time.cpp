395add67c1c7823b51f79e8b92a084b52e1e8bc6
AkankshRakesh <akankshrakesh@gmail.com>
2026-01-31 12:06:34 +0530

    Time complexity: O((E+V)logE), Space complexity: O(E + V)

--- 743. Network Delay Time.cpp
+++ 743. Network Delay Time.cpp
@@ -1,41 +1,47 @@
+using namespace std;
+
 class Solution {
 public:
-    struct Pair {
-        int x, y;
-        Pair(int _x, int _y) : x(_x), y(_y) {}
-    };
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int, int>>> adj;
+        vector<bool> visited(n + 1, false);
 
-    void dfs(unordered_map<int, vector<Pair>>& hm, int n, int ele, int eleCost, vector<int>& minCosts, int currCost) {
-        currCost += eleCost;
-        minCosts[ele] = min(minCosts[ele], currCost);
+        for (const auto& time : times) {
+            adj[time[0]].emplace_back(time[1], time[2]);
+        }
 
-        if (hm.count(ele)) {
-            for (auto& pair : hm[ele]) {
-                if (currCost + pair.y < minCosts[pair.x]) {
-                    dfs(hm, n, pair.x, pair.y, minCosts, currCost);
+        // Min-heap priority queue: pair<vertex, cost>, ordered by cost
+        priority_queue<pair<int, int>, vector<pair<int, int>>, 
+                       function<bool(pair<int,int>, pair<int,int>)>> q(
+            [](const pair<int,int>& a, const pair<int,int>& b) {
+                return a.second > b.second;
+            }
+        );
+
+        q.emplace(k, 0);
+
+        int ans = 0;
+        while (!q.empty()) {
+            auto [vertex, cost] = q.top();
+            q.pop();
+
+            if (visited[vertex]) continue;
+            visited[vertex] = true;
+            ans = max(ans, cost);
+
+            if (adj.find(vertex) == adj.end()) continue;
+
+            for (const auto& neig : adj[vertex]) {
+                if (!visited[neig.first]) {
+                    q.emplace(neig.first, cost + neig.second);
                 }
             }
         }
 
-        currCost -= eleCost; // Optional in C++, as currCost is passed by value
-    }
-
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> minCosts(n + 1, INT_MAX);
-        unordered_map<int, vector<Pair>> hm;
-
-        for (auto& time : times) {
-            int u = time[0], v = time[1], cost = time[2];
-            hm[u].emplace_back(v, cost);
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) return -1;
         }
 
-        dfs(hm, n, k, 0, minCosts, 0);
-
-        int maxNum = INT_MIN;
-        for (int i = 1; i <= n; ++i) {
-            maxNum = max(maxNum, minCosts[i]);
-        }
-
-        return maxNum == INT_MAX ? -1 : maxNum;
+        return ans;
     }
 };
