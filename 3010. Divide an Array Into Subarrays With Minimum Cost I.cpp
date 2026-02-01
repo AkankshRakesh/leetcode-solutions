a3e2da42fdf65474f42359a3228e790fdbf5dcef
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-01 10:10:34 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ 3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    public int minimumCost(int[] nums) {
-        int first = nums[1] > nums[2] ? nums[2] : nums[1], second = nums[1] < nums[2] ? nums[2] : nums[1];
-        for(int i = 3; i < nums.length; i++){
-            if(nums[i] < first){
+public:
+    int minimumCost(const std::vector<int>& nums) {
+        int first = std::min(nums[1], nums[2]);
+        int second = std::max(nums[1], nums[2]);
+        for (size_t i = 3; i < nums.size(); i++) {
+            if (nums[i] < first) {
                 second = first;
                 first = nums[i];
-            }
-            else if(nums[i] < second){
+            } else if (nums[i] < second) {
                 second = nums[i];
             }
         }
-
         return nums[0] + first + second;
     }
-}
+};
