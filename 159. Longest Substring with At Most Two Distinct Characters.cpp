c9780335e412718ca13691d7c36cc776efd9595b
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-02 21:37:12 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 159. Longest Substring with At Most Two Distinct Characters.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(const std::string& s, int k) {
+        if (k == 0) return 0;
+        std::vector<int> freq(128, 0);
+        int left = 0;
+        int ans = 0;
+        int count = 0;
+
+        for (int right = 0; right < (int)s.size(); right++) {
+            freq[s[right]]++;
+            if (freq[s[right]] == 1) count++;
+            while (count > k && left < right) {
+                freq[s[left]]--;
+                if (freq[s[left++]] == 0) count--;
+            }
+
+            ans = std::max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
