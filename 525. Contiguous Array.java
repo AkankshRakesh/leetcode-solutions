88fa926ae437546d1ff6183dc8f236696f1b5343
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-05 17:50:43 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 525. Contiguous Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, -1);
+        int sum = 0;
+        int ans = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(nums[i] == 0) sum -= 1;
+
+            if(!hm.containsKey(sum)) hm.put(sum, i);
+            else{
+                ans = Math.max(ans, i - hm.get(sum));
+            }
+        }
+
+        return ans;
+    }
+}
