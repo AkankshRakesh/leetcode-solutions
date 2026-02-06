ca17f864e3dbdc91c17310a807bc10bfd8599ee1
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-06 21:28:36 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 3634. Minimum Removals to Balance Array.java
+++ 3634. Minimum Removals to Balance Array.java
@@ -1,15 +1,16 @@
 class Solution {
     public int minRemoval(int[] nums, int k) {
         Arrays.sort(nums);
+
         int left = 0;
-        int n = nums.length;
-        int maxSize = 1;
-        for(int right = 1; right < n; right++){
-            while(left < right && (long) nums[left] * k < nums[right]) left++;
+        int ans = 0;
+        for(int right = 0; right < nums.length; right++){
+            while(nums[right] > (long)nums[left] * k && left < right) left++;
 
-            maxSize = Math.max(right - left + 1, maxSize);
+            ans = Math.max(ans, right - left + 1);
         }
+        // ans = Math.max(ans, nums.length - left - 1);
 
-        return n - maxSize;
+        return nums.length - ans;
     }
 }
