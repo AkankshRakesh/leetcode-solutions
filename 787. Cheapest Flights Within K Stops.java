2da894f9f315f0db22da94b0a5402ccea5cee525
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-10 17:21:00 +0530

    Time complexity: O(v*e(logv)), Space complexity: O(v + e)

--- 787. Cheapest Flights Within K Stops.java
+++ 787. Cheapest Flights Within K Stops.java
@@ -8,31 +8,31 @@ class Solution {
         }
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-        k++;
-        boolean[] visited = new boolean[n];
-        int[][] dp = new int[n][k + 2];
-        for(int i = 0; i < n; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
-
         pq.offer(new int[]{src, 0, 0});
-        dp[src][0] = 0;
-        while(!pq.isEmpty()){
-            int[] node = pq.poll();
-            int vertex = node[0];
-            int cost = node[1];
-            int stops = node[2];
-            if (cost > dp[vertex][stops]) continue;
-            if(stops > k) continue;
-            if(vertex == dst) return cost;
+        int[][] visited = new int[n][k + 2];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(visited[i], Integer.MAX_VALUE);
+        }
+        visited[src][0] = 0;
 
-            
-            if(!adj.containsKey(vertex)) continue;
-            for(int[] neig : adj.get(vertex)){
-                int nextCost = neig[1] + cost;
-                int nextStop = stops + 1;
-                
-                if(nextCost < dp[neig[0]][nextStop]){
-                    dp[neig[0]][nextStop] = nextCost;
-                    pq.offer(new int[]{neig[0], neig[1] + cost, stops + 1});
+        while(!pq.isEmpty()){
+            int node = pq.peek()[0];
+            int cost = pq.peek()[1];
+            int stops = pq.poll()[2];
+
+            if(stops > k + 1) continue;
+            if(node == dst) return cost;
+            if(!adj.containsKey(node)) continue;
+
+            for(int[] neig : adj.get(node)){
+                int nextNode = neig[0];
+                int nextCost = cost + neig[1];
+                int nextStops = stops + 1;
+                if(nextStops > k + 1) continue;
+
+                if(visited[nextNode][nextStops] > nextCost){
+                    pq.offer(new int[]{nextNode, nextCost, nextStops});
+                    visited[nextNode][nextStops] = nextCost;
                 }
             }
         }
