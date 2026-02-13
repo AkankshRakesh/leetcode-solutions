7355594188a9af8ac6d3ef04d91a3db3e56ad6e0
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-13 19:42:18 +0530

    Time complexity: O(elogv), Space complexity: O(v + e)

--- 743. Network Delay Time.java
+++ 743. Network Delay Time.java
@@ -1,35 +1,41 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
         HashMap<Integer, ArrayList<int[]>> adj = new HashMap<>();
-        boolean[] visited = new boolean[n + 1];
-        
         for(int[] time : times){
             ArrayList<int[]> temp = adj.getOrDefault(time[0], new ArrayList<>());
             temp.add(new int[]{time[1], time[2]});
             adj.put(time[0], temp);
         }
 
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-        q.offer(new int[]{k, 0});
+        pq.offer(new int[]{k, 0});
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, -1);
+        visited[k] = 0;
+        while(!pq.isEmpty()){
+            int size = pq.size();
+            for(int i = 0; i < size; i++){
+                int node = pq.peek()[0];
+                int cost = pq.poll()[1];
 
-        int ans = 0;
-        while(!q.isEmpty()){
-            int[] node = q.poll();
-            int vertex = node[0];
-            int cost = node[1];
-            
-            if(visited[vertex]) continue;
-            visited[vertex] = true;
-            ans = Math.max(ans, cost);
-            // System.out.println(vertex + "-" + cost);
-            if(!adj.containsKey(vertex)) continue;
+                if(!adj.containsKey(node)) continue;
+                for(int[] neig : adj.get(node)){
+                    int nextNode = neig[0];
+                    int nextCost = neig[1] + cost;
 
-            for(int[] neig : adj.get(vertex)){
-                if(!visited[neig[0]]) q.offer(new int[]{neig[0], cost + neig[1]});
+                    if(visited[nextNode] == -1 || visited[nextNode] > nextCost){
+                        visited[nextNode] = nextCost;
+                        pq.offer(new int[]{nextNode, nextCost});
+                    }
+                }
             }
         }
 
-        for(int i = 1; i <= n; i++) if(!visited[i]) return -1;
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            if(visited[i] == -1) return -1;
+            ans = Math.max(ans, visited[i]);
+        }
 
         return ans;
     }
