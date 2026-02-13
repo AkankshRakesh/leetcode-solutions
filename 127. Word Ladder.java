c0c86614567a91cf135a6fa4e88671d59fb6f67a
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-13 11:26:36 +0530

    Time complexity: O(n*26), Space complexity: O(n)

--- 127. Word Ladder.java
+++ 127. Word Ladder.java
@@ -1,30 +1,39 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        for(String word : wordList) hm.put(word, -1);
-        Queue<String> q = new LinkedList<>();
-        q.offer(beginWord);
-        hm.put(beginWord, 1);
-
-        while(!q.isEmpty()){
-            String word = q.poll();
-            for(int i = 0; i < word.length(); i++){
-                StringBuilder sb = new StringBuilder(i == 0 ? "" : word.substring(0, i));
-                for(int j = 0; j < 26; j++){
-                    StringBuilder temp = new StringBuilder(sb);
-                    char ch = (char)(j + 'a');
-                    temp.append(ch);
-                    temp.append(word.substring(i + 1, word.length()));
-                    if(hm.containsKey(temp.toString()) && hm.get(temp.toString()) == -1){
-                        hm.put(temp.toString(), hm.get(word) + 1);
-                        q.offer(temp.toString());
-                        // System.out.println(word + "-" + hm.get(word));
-                    }
+    public void addVariations(String word, HashMap<String, Integer> hm, boolean[] visited, Queue<String> q){
+        for(int i = 0; i < word.length(); i++){
+            StringBuilder sb = new StringBuilder(word);
+            for(int j = 0; j < 27; j++){
+                char ch = (char)('a' + j);
+                sb.setCharAt(i, ch);
+                String temp = sb.toString();
+                if(hm.containsKey(temp) && !visited[hm.get(temp)]){
+                    visited[hm.get(temp)] = true;
+                    q.offer(temp);
                 }
             }
         }
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
+        int index = 0;
+        for(String word : wordList) hm.put(word, index++);
 
-        if(!hm.containsKey(endWord) || hm.get(endWord) == -1) return 0;
-        return hm.get(endWord);
+        if(!hm.containsKey(endWord)) return 0;
+        boolean[] visited = new boolean[wordList.size()];
+
+        int ans = 1;
+        addVariations(beginWord, hm, visited, q);
+        while(!q.isEmpty()){
+            int size = q.size();
+            ans++;
+            for(int i = 0; i < size; i++){
+                String word = q.poll();
+                if(word.equals(endWord)) return ans;
+                addVariations(word, hm, visited, q);
+            }
+        }
+
+        return 0;
     }
 }
