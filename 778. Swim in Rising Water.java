a44b428ab57de7d7032321ea4d37ec04d1304fe0
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-13 18:40:40 +0530

    Time complexity: O(n*m*logn), Space complexity: O(n)

--- 778. Swim in Rising Water.java
+++ 778. Swim in Rising Water.java
@@ -1,25 +1,24 @@
 class Solution {
-    public boolean dfs(int[][] grid, int i, int j, int cost, boolean[][] visited){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] || grid[i][j] > cost) return false;
+    public boolean dfs(int[][] grid, int i, int j, int max, boolean[][] visited){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] > max || visited[i][j]) return false;
         if(i == grid.length - 1 && j == grid[0].length - 1) return true;
-
         visited[i][j] = true;
-        return dfs(grid, i + 1, j, cost, visited) | dfs(grid, i - 1, j, cost, visited) | dfs(grid, i, j + 1, cost, visited) | dfs(grid, i, j - 1, cost, visited);
+        
+        return dfs(grid, i + 1, j, max, visited) | 
+        dfs(grid, i - 1, j, max, visited) |
+        dfs(grid, i, j + 1, max, visited) |
+        dfs(grid, i, j - 1, max, visited);
     }
     public int swimInWater(int[][] grid) {
-        int left = 0, right = grid.length * grid.length;
-        int ans = Integer.MAX_VALUE;
-
+        int left = 0;
+        int right = grid.length * grid.length;
         while(left < right){
+            boolean[][] visited = new boolean[grid.length][grid.length];
             int mid = left + (right - left) / 2;
-            boolean[][] visited = new boolean[grid.length][grid[0].length];
-            if(dfs(grid, 0, 0, mid, visited)){
-                ans = mid;
-                right = mid;
-            }
+            if(dfs(grid, 0, 0, mid, visited)) right = mid;
             else left = mid + 1;
         }
 
-        return ans;
+        return left;
     }
 }
