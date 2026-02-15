6f20a3b5257af59e43ea17a58a484a71c3e736a6
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-15 19:20:54 +0530

    Time complexity: O(n^2 * logn), Space complexity: O(n^2)

--- 1584. Min Cost to Connect All Points.cpp
+++ 1584. Min Cost to Connect All Points.cpp
@@ -1,38 +1,62 @@
 class Solution {
-    struct Pair {
-        int point;
-        int dist;
-        Pair(int p, int d) : point(p), dist(d) {}
-    };
-public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<bool> visited(n, false);
-
-        auto cmp = [](const Pair& a, const Pair& b) {
-            return a.dist > b.dist;
-        };
-        priority_queue<Pair, vector<Pair>, decltype(cmp)> pq(cmp);
-
-        int ans = 0;
-        pq.push(Pair(0, 0));
-
-        while (!pq.empty()) {
-            Pair p = pq.top();
-            pq.pop();
-            if (visited[p.point]) continue;
-
-            ans += p.dist;
-            visited[p.point] = true;
-
-            for (int neig = 0; neig < n; neig++) {
-                if (neig == p.point || visited[neig]) continue;
-                int dist = abs(points[p.point][0] - points[neig][0]) +
-                           abs(points[p.point][1] - points[neig][1]);
-                pq.push(Pair(neig, dist));
+    class DSU {
+        vector<int> parent;
+        vector<int> rank;
+    public:
+        DSU(int n) : parent(n), rank(n, 1) {
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
         }
 
-        return ans;
+        int find(int node) {
+            if (parent[node] != node) 
+                parent[node] = find(parent[node]);
+            return parent[node];
+        }
+
+        void unionSets(int n1, int n2) {
+            int p1 = find(n1);
+            int p2 = find(n2);
+            if (p1 == p2) return;
+
+            if (rank[p1] > rank[p2]) parent[p2] = p1;
+            else if (rank[p1] < rank[p2]) parent[p1] = p2;
+            else {
+                parent[p2] = p1;
+                rank[p1]++;
+            }
+        }
+    };
+
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        using Edge = vector<int>;
+        auto cmp = [](const Edge& a, const Edge& b) { return a[2] > b[2]; };
+        priority_queue<Edge, vector<Edge>, decltype(cmp)> pq(cmp);
+
+        int n = points.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                pq.push({i, j, dist});
+            }
+        }
+
+        int cost = 0;
+        DSU dsu(n);
+        while (!pq.empty()) {
+            auto edge = pq.top();
+            pq.pop();
+            int u = edge[0];
+            int v = edge[1];
+            int edgeCost = edge[2];
+            if (dsu.find(u) != dsu.find(v)) {
+                dsu.unionSets(u, v);
+                cost += edgeCost;
+            }
+        }
+
+        return cost;
     }
 };
