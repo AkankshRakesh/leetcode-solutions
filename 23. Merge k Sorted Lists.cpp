5cf605ff51bf6c8e57f4ff03e5282dedc5c0ac02
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-17 21:28:49 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -1,41 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* ans = new ListNode();
-        int k = lists.size();
-        ListNode* headAns = ans;
+        ListNode* ansHead = ans;
+        auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
 
-        while (true) {
-            int leastInd = -1;
-            for (int i = 0; i < k; i++) {
-                ListNode* head = lists[i];
-                if (head == nullptr) continue;
-
-                if (leastInd == -1 || lists[leastInd]->val > lists[i]->val)
-                    leastInd = i;
+        for (ListNode* head : lists) {
+            while (head != nullptr) {
+                pq.push(new ListNode(head->val));
+                head = head->next;
             }
-            if (leastInd == -1) break;
-
-            if (ans == nullptr) {
-                ans = new ListNode(lists[leastInd]->val);
-            } else {
-                ans->next = new ListNode(lists[leastInd]->val);
-                ans = ans->next;
-            }
-
-            lists[leastInd] = lists[leastInd]->next;
         }
 
-        return headAns->next;
+        while (!pq.empty()) {
+            if (ans == nullptr) ans = pq.top();
+            else {
+                ans->next = pq.top();
+                ans = ans->next;
+            }
+            pq.pop();
+        }
+
+        return ansHead->next;
     }
 };
