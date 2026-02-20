8cba9ccc3a823ae89f6f2ec7b0bf2c1ea66881a0
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-20 19:36:49 +0530

    Time complexity: O(n + m), Space complexity: O(1)

--- 76. Minimum Window Substring.cpp
+++ 76. Minimum Window Substring.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        int ansFreq[128] = {0};
-        int ansCount = 0;
+    string minWindow(const string& s, const string& t) {
+        int needed[128] = {0};
+        int count = 0;
         for (char c : t) {
-            if (ansFreq[c] == 0) ansCount++;
-            ansFreq[c]++;
+            needed[c]++;
+            count++;
         }
 
-        int freq[128] = {0};
-        int count = 0;
-        string ans = s;
+        int ansLeft = 0, ansRight = 0;
         int left = 0;
-        bool oneMatch = false;
-
+        int len = INT_MAX;
         for (int right = 0; right < (int)s.size(); right++) {
-            freq[s[right]]++;
-            if (freq[s[right]] == ansFreq[s[right]]) count++;
+            if (needed[s[right]] > 0) count--;
+            needed[s[right]]--;
 
-            while (count == ansCount) {
-                oneMatch = true;
-                if (right - left + 1 < (int)ans.size()) {
-                    ans = s.substr(left, right - left + 1);
+            while (count == 0) {
+                if (right - left < len) {
+                    len = right - left;
+                    ansLeft = left;
+                    ansRight = right;
                 }
-                if (freq[s[left]] == ansFreq[s[left]]) count--;
-                freq[s[left]]--;
+                if (needed[s[left]] == 0) count++;
+                needed[s[left]]++;
                 left++;
             }
         }
 
-        if (!oneMatch) return "";
-        return ans;
+        return len == INT_MAX ? "" : s.substr(ansLeft, ansRight - ansLeft + 1);
     }
 };
+
