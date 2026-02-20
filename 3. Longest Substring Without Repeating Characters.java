afb52853d7a5cfb94326da727a366d997f576767
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-20 19:37:17 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 3. Longest Substring Without Repeating Characters.java
+++ 3. Longest Substring Without Repeating Characters.java
@@ -1,16 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[128];
         int left = 0;
-        boolean[] check = new boolean[256];
         int ans = 0;
-
         for(int right = 0; right < s.length(); right++){
-            while(check[s.charAt(right)]){
-                check[s.charAt(left)] = false;
+            freq[s.charAt(right)]++;
+
+            while(freq[s.charAt(right)] >= 2){
+                freq[s.charAt(left)]--;
                 left++;
             }
+
             ans = Math.max(ans, right - left + 1);
-            check[s.charAt(right)] = true;
         }
 
         return ans;
