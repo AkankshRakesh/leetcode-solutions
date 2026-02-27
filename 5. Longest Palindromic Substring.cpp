c41f08c321a50769a235e2dd7e6d4026e5889c5b
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-27 19:33:27 +0530

    Time complexity: O(n^2), Space complexity: O(1)

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        string sb = "#";
-        for (char ch : s) {
-            sb += ch;
-            sb += "#";
+    vector<int> expand(const string& s, int left, int right) {
+        while (left >= 0 && right < (int)s.length() && s[left] == s[right]) {
+            left--;
+            right++;
         }
+        return {left + 1, right - 1};
+    }
 
-        int ansLen = 0;
-        int ansCenter = 0;
-
-        for (int i = 0; i < (int)sb.size(); i++) {
-            int left = i - 1, right = i + 1;
-            int len = (sb[i] == '#') ? 0 : 1;
-
-            while (left >= 0 && right < (int)sb.size() && sb[left] == sb[right]) {
-                if (sb[left] != '#') len += 2;
-                left--;
-                right++;  
+    string longestPalindrome(const string& s) {
+        int len = 0, ansLeft = 0, ansRight = 0;
+        for (int i = 1; i < (int)s.length(); i++) {
+            vector<int> boundary1 = expand(s, i - 1, i + 1);
+            vector<int> boundary2(2, 0);
+            if (s[i] == s[i - 1]) boundary2 = expand(s, i - 2, i + 1);
+            int len1 = boundary1[1] - boundary1[0];
+            int len2 = boundary2[1] - boundary2[0];
+            if (len < len1 || len < len2) {
+                if (len1 < len2) {
+                    len = len2;
+                    ansLeft = boundary2[0];
+                    ansRight = boundary2[1];
+                } else {
+                    len = len1;
+                    ansLeft = boundary1[0];
+                    ansRight = boundary1[1];
+                }
             }
-
-            if (len > ansLen) {
-                ansCenter = i;
-                ansLen = len;
-            }
         }
 
-        int start = ansCenter - ansLen;
-        int end = ansCenter + ansLen;
-        string result = "";
-
-        for (int i = start; i <= end; i++) {
-            if (sb[i] != '#') result += sb[i];
-        }
-
-        return result;
+        return s.substr(ansLeft, ansRight - ansLeft + 1);
     }
 };
+
