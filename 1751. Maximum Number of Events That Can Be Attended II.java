2b17f8acf79f9c61418f8f2b842694b5b20931e2
AkankshRakesh <akankshrakesh@gmail.com>
2026-02-28 17:50:31 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

--- 1751. Maximum Number of Events That Can Be Attended II.java
+++ 1751. Maximum Number of Events That Can Be Attended II.java
@@ -1,39 +1,31 @@
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length;
-        
-        dp = new int[k + 1][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+    public int findIndex(int[][] rides, int index){
+        int left = index + 1, right = rides.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(rides[mid][0] > rides[index][1]) right = mid - 1;
+            else left = mid + 1;
         }
-        
-        return dfs(0, k, events);
-    }    
-    
-    private int[][] dp;
-    private int dfs(int curIndex, int count, int[][] events) {
-        if (count == 0 || curIndex == events.length) {
-            return 0;
-        }
-        if (dp[count][curIndex] != -1) {
-            return dp[count][curIndex];
-        }
-        int nextIndex = bisectRight(events, events[curIndex][1]);
-        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
-        return dp[count][curIndex];
-    }
-    
-    public static int bisectRight(int[][] events, int target) {
-        int left = 0, right = events.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (events[mid][0] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
+
         return left;
-    }   
+    }
+
+    public int solve(int[][] events, int k, int[][] dp, int index){
+        if(index >= events.length || k == 0) return 0;
+        if(dp[index][k] != -1) return dp[index][k];
+
+        int nextInd = findIndex(events, index);
+        int pick = events[index][2] + solve(events, k - 1, dp, nextInd);
+        int notPick = solve(events, k, dp, index + 1);
+
+        return dp[index][k] = Math.max(pick, notPick);
+    }
+
+    public int maxValue(int[][] events, int k) {
+        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+        int[][] dp = new int[events.length][k + 1];
+        for(int i = 0; i < events.length; i++) Arrays.fill(dp[i], -1);
+
+        return solve(events, k, dp, 0);
+    }
 }
