f5452ebdb254d44c664dc868fb1332a107b28617
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-07 17:22:01 +0530

    Time complexity: O(logn), Space complexity: O(1)

--- 700. Search in a Binary Search Tree.cpp
+++ 700. Search in a Binary Search Tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution {  
 public:
     void bst(TreeNode* root, int val){
         
