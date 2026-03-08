163b18f0c93719166eb702ba97aa0372ed5549a9
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-08 10:44:33 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

--- 300. Longest Increasing Subsequence - OPT.java
+++ 300. Longest Increasing Subsequence - OPT.java
@@ -1,18 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-        for(int i = 1; i < nums.length; i++){
-            if(arr.get(arr.size() - 1) < nums[i]) arr.add(nums[i]);
+        for(int num : nums){
+            if(arr.size() == 0 || arr.get(arr.size() - 1) < num) arr.add(num);
             else{
-                int left = 0;
-                int right = arr.size();
-                while(left < right){
+                int left = 0, right = arr.size() - 1;
+                int ans = 0;
+                while(left <= right){
                     int mid = left + (right - left) / 2;
-                    if(arr.get(mid) >= nums[i]) right = mid;
-                    else left = mid + 1;
+                    if(arr.get(mid) < num) left = mid + 1;
+                    else{
+                        ans = mid;
+                        right = mid - 1;
+                    }
                 }
-                if(left < arr.size()) arr.set(left, nums[i]);
+                arr.set(ans, num);
             }
         }
 
