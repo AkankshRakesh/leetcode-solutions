7fc0fd8c208e8490ee5852b677de408f426e3b21
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-08 10:44:05 +0530

    Time complexity: O(nlogn), Space complexity: O(n)

--- 300. Longest Increasing Subsequence -OPT.cpp
+++ 300. Longest Increasing Subsequence -OPT.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> arr;
-        arr.push_back(nums[0]);
-        for (int i = 1; i < (int)nums.size(); i++) {
-            if (arr.back() < nums[i]) arr.push_back(nums[i]);
-            else {
-                int left = 0;
-                int right = (int)arr.size();
-                while (left < right) {
+        for (int num : nums) {
+            if (arr.empty() || arr.back() < num) {
+                arr.push_back(num);
+            } else {
+                int left = 0, right = (int)arr.size() - 1;
+                int ans = 0;
+                while (left <= right) {
                     int mid = left + (right - left) / 2;
-                    if (arr[mid] >= nums[i]) right = mid;
-                    else left = mid + 1;
+                    if (arr[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        ans = mid;
+                        right = mid - 1;
+                    }
                 }
-                if (left < (int)arr.size()) arr[left] = nums[i];
+                arr[ans] = num;
             }
         }
         return (int)arr.size();
