4835dfbc8a78306810854a8036beff6d09605cb2
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-08 08:54:48 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 1980. Find Unique Binary String.java
+++ 1980. Find Unique Binary String.java
@@ -1,30 +1,15 @@
 class Solution {
-    String ans;
-    public boolean backtrack(StringBuilder curr, int len, HashSet<String> hs){
-        if(curr.length() == len){
-            if(!hs.contains(curr.toString())){
-                ans = curr.toString();
-                return true;
-            }
-            return false;
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder ans = new StringBuilder();
+        int n = nums.length;
+        int index = 0;
+        for(String num : nums){
+            if(num.charAt(index) == '0') ans.append('1');
+            else ans.append('0');
+
+            index++;
         }
 
-        curr.append("0");
-        boolean res = backtrack(curr, len, hs);
-        curr.deleteCharAt(curr.length() - 1);
-        if(res) return res;
-
-        curr.append("1");
-        res = backtrack(curr, len, hs);
-        curr.deleteCharAt(curr.length() - 1);
-
-        return res;
-    }
-    public String findDifferentBinaryString(String[] nums) {
-        HashSet<String> hs = new HashSet<>();
-        for(String s : nums) hs.add(s);
-
-        backtrack(new StringBuilder(), nums[0].length(), hs);
-        return ans;
+        return ans.toString();
     }
 }
