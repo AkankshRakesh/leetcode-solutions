4254d31cf614f46ca852bebe3b109f94991e322b
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-11 13:47:36 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 41. First Missing Positive.cpp
+++ 41. First Missing Positive.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-        
         int n = nums.size();
-        nums.push_back(0);
-        int smallest = n;
-        for (int i = 0 ; i < n; ++i)
-        {
-            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]])
-                swap(nums[i], nums[nums[i]]);
+        for(int i = 0; i < n; i++){
+            while(nums[i] >= 1 && nums[i] <= n && nums[i] - 1 != i && nums[nums[i] - 1] != nums[i]){
+                swap(nums[nums[i] - 1], nums[i]);
+            }
         }
-        for (int i = 1; i <= n; ++i)
-            if (nums[i] != i)
-                return i;
+        for(int i = 0; i < n; i++){
+            if(nums[i] - 1 != i) return i + 1;
+        }
         return n + 1;
     }
 };
