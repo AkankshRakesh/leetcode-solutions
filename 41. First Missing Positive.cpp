b412249ae3c8c4bbc2afeb294291d06518e04e64
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-11 13:49:49 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 41. First Missing Positive.cpp
+++ 41. First Missing Positive.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            while(nums[i] >= 1 && nums[i] <= n && nums[i] - 1 != i && nums[nums[i] - 1] != nums[i]){
-                swap(nums[nums[i] - 1], nums[i]);
-            }
+        nums.push_back(0);
+        for (int i = 0 ; i < n; ++i)
+        {
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]])
+                swap(nums[i], nums[nums[i]]);
         }
-        for(int i = 0; i < n; i++){
-            if(nums[i] - 1 != i) return i + 1;
-        }
+        for (int i = 1; i <= n; ++i)
+            if (nums[i] != i)
+                return i;
         return n + 1;
     }
 };
