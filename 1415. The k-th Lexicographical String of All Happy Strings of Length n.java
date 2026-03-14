494b7b992b5fabcd8fe9b8d7ff880eac27e0191f
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-14 11:14:03 +0530

    Time complexity: O(n^2 * 2^n), Space complexity: O(n*2^n)

--- 1415. The k-th Lexicographical String of All Happy Strings of Length n.java
+++ 1415. The k-th Lexicographical String of All Happy Strings of Length n.java
@@ -1,31 +1,39 @@
 class Solution {
-
-    List<String> goodStrings = new ArrayList<>();
-
-    public String getHappyString(int n, int k) {
-        String currString = "";
-        generategoodStrings(n, currString);
-
-        if (goodStrings.size() < k) return "";
-
-        Collections.sort(goodStrings);
-
-        return goodStrings.get(k - 1);
-    }
-
-    public void generategoodStrings(int n, String currString) {
-        if (currString.length() == n) {
-            goodStrings.add(currString);
+    public void buildStrs(int n, String[] happyStrs, StringBuilder sb, int[] index){
+        if(sb.length() == n){
+            happyStrs[index[0]] = new String(sb.toString());
+            index[0]++;
             return;
         }
 
-        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
-            if (
-                currString.length() > 0 &&
-                currString.charAt(currString.length() - 1) == currentChar
-            ) continue;
-
-            generategoodStrings(n, currString + currentChar);
+        if(sb.charAt(sb.length() - 1) != 'b'){
+            sb.append('b');
+            buildStrs(n, happyStrs, sb, index);
+            sb.deleteCharAt(sb.length() - 1);
         }
+        if(sb.charAt(sb.length() - 1) != 'a'){
+            sb.append('a');
+            buildStrs(n, happyStrs, sb, index);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if(sb.charAt(sb.length() - 1) != 'c'){
+            sb.append('c');
+            buildStrs(n, happyStrs, sb, index);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+    public String getHappyString(int n, int k) {
+        int totalStrs = 3;
+        for(int i = 1; i < n; i++) totalStrs *= 2;
+        if(k > totalStrs) return "";
+        String[] happyStrs = new String[totalStrs];
+
+        int[] index = new int[]{0};
+        buildStrs(n, happyStrs, new StringBuilder("a"), index);
+        buildStrs(n, happyStrs, new StringBuilder("b"), index);
+        buildStrs(n, happyStrs, new StringBuilder("c"), index);
+
+        Arrays.sort(happyStrs);
+        return happyStrs[k - 1];
     }
 }
