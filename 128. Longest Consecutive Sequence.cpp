b1743d08088f35e0e674e2453683313d4b066b57
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-28 13:06:20 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    int recurse(unordered_map<int, int>& hm, vector<bool>& visited, vector<int>& seq, int key) {
+        if (hm.find(key) == hm.end()) return 0;
+        if (visited[hm[key]]) return seq[hm[key]];
+
+        visited[hm[key]] = true;
+        return seq[hm[key]] = 1 + recurse(hm, visited, seq, key + 1);
+    }
+
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        int longest = 0;
+        if (nums.empty()) return 0;
+        unordered_map<int, int> hm;
+        vector<bool> visited(nums.size(), false);
+        vector<int> seq(nums.size(), 0);
+        int ans = 1;
 
-        for (int num : numSet) {
-            if (numSet.find(num - 1) == numSet.end()) {
-                int length = 1;
-
-                while (numSet.find(num + length) != numSet.end()) {
-                    length++;
-                }
-
-                longest = max(longest, length);
-            }
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (hm.find(nums[i]) == hm.end()) hm[nums[i]] = i;
         }
 
-        return longest;
+        for (int num : nums) {
+            recurse(hm, visited, seq, num);
+            ans = max(ans, seq[hm[num]]);
+        }
+
+        return ans;
     }
 };
