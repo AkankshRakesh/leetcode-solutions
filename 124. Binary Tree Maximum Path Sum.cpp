b3d69cd18ecd8dbce434864bbb70eb8a211a1fad
AkankshRakesh <akankshrakesh@gmail.com>
2026-03-29 19:25:28 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 124. Binary Tree Maximum Path Sum.cpp
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -1,32 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */ 
 class Solution {
-    public int bfs(TreeNode node, int[] sum){
-        if(node == null) return 0;
+    int ans = INT_MIN;
+public:
+    int dfs(TreeNode* root) {
+        if (root == nullptr) return 0;
 
-        int left = Math.max(0, bfs(node.left, sum));
-        int right = Math.max(0, bfs(node.right, sum));
-        
-        sum[0] = Math.max(sum[0], left + right + node.val);
+        int left = dfs(root->left);
+        int right = dfs(root->right);
 
-        return Math.max(left, right) + node.val;
+        ans = max(ans, max(left + root->val + right, max(left + root->val, max(right + root->val, root->val))));
+
+        return max(root->val + left, max(root->val + right, root->val));
     }
-    public int maxPathSum(TreeNode root) {
-        int[] sum = { root.val };
-        bfs(root, sum);
-        return sum[0];
+
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return ans;
     }
-}
+};
