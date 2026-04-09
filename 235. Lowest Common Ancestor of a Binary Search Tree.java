3ea45328ebd9dce9ea953c643337911a60c0248b
AkankshRakesh <akankshrakesh@gmail.com>
2026-04-09 15:14:07 +0530

    Time complexity: O(h), Space complexity: O(h)

--- 235. Lowest Common Ancestor of a Binary Search Tree.java
+++ 235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -9,25 +9,19 @@
  */
 
 class Solution {
-    // public TreeNode dfs(TreeNode node, TreeNode p, TreeNode q){
-    //     if(node == null) return null;
-    //     if((node.val > p.val && node.val < q.val) || (node.val < p.val && node.val > q.val) || (node.val == p.val) || (node.val == q.val)) return node;
-
-    //     if(node.val > p.val) return dfs(node.left, p, q);
-    //     return dfs(node.right, p, q);
-    // }
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while(!queue.isEmpty()){
-            TreeNode node = queue.poll();
-            if((node.val > p.val && node.val < q.val) || (node.val < p.val && node.val > q.val) || (node.val == p.val) || (node.val == q.val)) return node;
-
-            if(node.val > p.val) queue.offer(node.left);
-            else queue.offer(node.right);
+    TreeNode ans = new TreeNode(-1);
+    public void dfs(TreeNode node, TreeNode p, TreeNode q){
+        if(node == null) return;
+        // System.out.println(node.val);
+        if((p.val > node.val && q.val < node.val) || (p.val < node.val && q.val > node.val) || (p.val == node.val) || (q.val == node.val)){
+            ans = node;
+            return;
         }
-
-        return root;
+        if(p.val < node.val) dfs(node.left, p, q);
+        else dfs(node.right, p, q);
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return ans;
     }
 }
