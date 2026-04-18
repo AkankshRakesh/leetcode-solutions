1f774d764f04b66f96de4ff9e444282d3e0d6235
AkankshRakesh <akankshrakesh@gmail.com>
2026-04-18 10:19:32 +0530

    Time complexity: O(1), Space complexity: O(1)

---
+++ 3783. Mirror Distance of an Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int rev = 0;
+        int temp = n;
+        while(n != 0){
+            rev *= 10;
+            rev += n % 10;
+            n /= 10;
+        }
+
+        return Math.abs(temp - rev);
+    }
+}
