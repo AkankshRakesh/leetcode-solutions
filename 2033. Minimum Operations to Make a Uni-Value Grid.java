f6fb185cd47427c7f6c284d22a015f989a1f66da
AkankshRakesh <akankshrakesh@gmail.com>
2026-04-28 21:53:08 +0530

    Time complexity: O(nm*log(nm)), Space complexity: O(nm)

--- 2033. Minimum Operations to Make a Uni-Value Grid.java
+++ 2033. Minimum Operations to Make a Uni-Value Grid.java
@@ -1,27 +1,26 @@
 class Solution {
-
     public int minOperations(int[][] grid, int x) {
-        ArrayList<Integer> numsArray = new ArrayList<>();
-        int result = 0;
-
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                numsArray.add(grid[row][col]);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                arr.add(grid[i][j]);
             }
         }
 
-        Collections.sort(numsArray);
-
-        int length = numsArray.size();
-        int finalCommonNumber = numsArray.get(length / 2);
-
-        for (int number : numsArray) {
-            if (number % x != finalCommonNumber % x) {
-                return -1;
+        Collections.sort(arr);
+        int mid = arr.get(arr.size() / 2);
+        int ans = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i) > mid){
+                if(((double)(arr.get(i) - mid) / x) != (arr.get(i) - mid) / x) return -1;
+                ans += (arr.get(i) - mid) / x;
             }
-            result += Math.abs(finalCommonNumber - number) / x;
+            else{
+                if(((double)(mid - arr.get(i)) / x) != (mid - arr.get(i)) / x) return -1;
+                ans += (mid - arr.get(i)) / x;
+            }
         }
 
-        return result;
+        return ans;
     }
 }
