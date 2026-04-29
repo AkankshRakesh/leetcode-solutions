475fe5a4eea0efc79d5d0c777c4cd2da4fc334c0
AkankshRakesh <akankshrakesh@gmail.com>
2026-04-29 17:22:00 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 103. Binary Tree Zigzag Level Order Traversal.java
+++ 103. Binary Tree Zigzag Level Order Traversal.java
@@ -15,32 +15,26 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        ArrayList<Integer> currArr = new ArrayList<>();
-        List<List<Integer>> arr = new ArrayList<>();
-        if(root == null) return arr;
+        
         boolean reverse = false;
-
         while(!q.isEmpty()){
             int size = q.size();
-            while(size != 0){
-                TreeNode ele = q.poll();
-                currArr.add(ele.val);
-                if(ele.left != null) q.add(ele.left);
-                if(ele.right != null) q.add(ele.right);
-                size--;
+            List<Integer> arr = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                arr.add(node.val);
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
-
-            if(reverse){
-                Collections.reverse(currArr);
-            }
+            if(reverse) Collections.reverse(arr);
             reverse = !reverse;
-            arr.add(new ArrayList<Integer>(currArr));
-            currArr = new ArrayList<>();
+            ans.add(arr);
         }
 
-        return arr;
-
+        return ans;
     }
 }
