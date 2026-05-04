e47bddc5cee5f3e281a92ca8f89f412f2dda21c2
AkankshRakesh <akankshrakesh@gmail.com>
2026-05-04 12:12:13 +0530

    Time complexity: O(n^2), Space complexity: O(1)

--- 48. Rotate Image.java
+++ 48. Rotate Image.java
@@ -1,17 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
+        int n = matrix.length, m = matrix[0].length;
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
+            for(int j = 0; j < m; j++){
+                if(i == j) break;
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
+        }
 
-            for(int j = 0; j < n / 2; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m / 2; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+                matrix[i][j] = matrix[i][m - j - 1];
+                matrix[i][m - j - 1] = temp;
             }
         }
     }
