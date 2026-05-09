1e03266c6261d9ca3ccf016e1bef9dfa71717d41
AkankshRakesh <akankshrakesh@gmail.com>
2026-05-09 19:23:44 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 538. Convert BST to Greater Tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public void solve(TreeNode node){
+        if(node == null) return;
+
+        solve(node.right);
+        int prev = node.val;
+        node.val += sum;
+        sum += prev;
+        solve(node.left);
+    }
+    public TreeNode convertBST(TreeNode root) {
+        solve(root);
+        return root;
+    }
+}
