6b95313451671684f1b8f627dd1eb260f43b4808
AkankshRakesh <akankshrakesh@gmail.com>
2026-05-15 19:36:10 +0530

    Time complexity: O(logn), Space complexity: O(1)

--- 153. Find Minimum in Rotated Sorted Array.java
+++ 153. Find Minimum in Rotated Sorted Array.java
@@ -2,14 +2,13 @@ class Solution {
     public int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        int min = Integer.MAX_VALUE;
+        // int min = Integer.MAX_VALUE;
 
         while(left < right){
             int mid = left + (right - left) / 2;
-            System.out.println(mid);
+            // System.out.println(mid);
             if(nums[mid] > nums[right]) left = mid + 1;
             else{
-                min = Math.min(min, nums[mid]);
                 right = mid;
             }
         }
