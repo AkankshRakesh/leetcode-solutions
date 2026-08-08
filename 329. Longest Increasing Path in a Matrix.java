063d4d6818d98a1c6b86280d8e42090a2c5bae53
AkankshRakesh <akankshrakesh@gmail.com>
2026-08-08 16:18:07 +0530

    Time complexity: O(n * n), Space complexity: O(n * n)

--- 329. Longest Increasing Path in a Matrix.java
+++ 329. Longest Increasing Path in a Matrix.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int dfs(int[][] matrix, int i, int j, int[][] dp, boolean[][] visited, int prev){
-        if(i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] <= prev) return 0;
-        if(dp[i][j] != 0) return dp[i][j];
+    public int dfs(int[][] matrix, int i, int j, int past, int[][] dp, boolean[][] visited){
+        if(i >= matrix.length || j >= matrix[0].length || i < 0 || j < 0 || matrix[i][j] <= past || visited[i][j]) return 0;
 
-        return dp[i][j] = 1 + Math.max(dfs(matrix, i + 1, j, dp, visited, matrix[i][j]), Math.max(dfs(matrix, i - 1, j, dp, visited, matrix[i][j]), Math.max(dfs(matrix, i, j + 1, dp, visited, matrix[i][j]), dfs(matrix, i, j - 1, dp, visited, matrix[i][j]))));
+        if(dp[i][j] != -1) return dp[i][j];
+        visited[i][j] = true;
+
+        int res = 0;
+        res = Math.max(res, 1 + dfs(matrix, i + 1, j, matrix[i][j], dp, visited));
+        res = Math.max(res, 1 + dfs(matrix, i - 1, j, matrix[i][j], dp, visited));
+        res = Math.max(res, 1 + dfs(matrix, i, j + 1, matrix[i][j], dp, visited));
+        res = Math.max(res, 1 + dfs(matrix, i, j - 1, matrix[i][j], dp, visited));
+
+        visited[i][j] = false;
+
+        return dp[i][j] = res;
     }
     public int longestIncreasingPath(int[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
-        int max = 1;
+        int n = matrix.length, m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-                dfs(matrix, i, j, dp, visited, Integer.MIN_VALUE);
-                max = Math.max(max, dp[i][j]);
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        int ans = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                ans = Math.max(ans, dfs(matrix, i, j, -1, dp, visited));
             }
         }
 
-        return max;
+        return ans;
     }
 }
