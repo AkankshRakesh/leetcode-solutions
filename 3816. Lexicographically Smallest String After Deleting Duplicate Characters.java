b6044ae5168c4ed2a594d835f9a646a9bccba85d
AkankshRakesh <akankshrakesh@gmail.com>
2026-08-20 18:56:34 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 3816. Lexicographically Smallest String After Deleting Duplicate Characters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String lexSmallestAfterDeletion(String s) {
+        int[] freq = new int[128];
+        for(int i = 0; i < s.length(); i++){
+            freq[s.charAt(i)]++;
+        }
+
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            while(st.size() > 0 && st.peek() > s.charAt(i) && freq[st.peek()] > 1){
+                freq[st.peek()]--;
+                st.pop();
+            }
+            st.push(s.charAt(i));
+        }
+
+        char dup = st.peek();
+        while(st.size() > 0 && freq[st.peek()] > 1){
+            freq[st.peek()]--;
+            st.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) sb.append(st.pop());
+
+        return sb.reverse().toString();
+    }
+}
