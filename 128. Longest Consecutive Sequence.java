281fc83ec463448320bbebab313337379cd1f19b
AkankshRakesh <akankshrakesh@gmail.com>
2026-08-21 17:28:33 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,24 +1,25 @@
 class Solution {
-    public int recurse(HashMap<Integer, Integer> hm, boolean[] visited, int[] seq, int key){
-        if(!hm.containsKey(key)) return 0;
-        if(visited[hm.get(key)]) return seq[hm.get(key)];
-
-        visited[hm.get(key)] = true;
-        return seq[hm.get(key)] = 1 + recurse(hm, visited, seq, key + 1);
-    }
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        boolean[] visited = new boolean[nums.length];
-        int[] seq = new int[nums.length];
-        int ans = 1;
-        for(int i = 0; i < nums.length; i++){
-            if(!hm.containsKey(nums[i])) hm.put(nums[i], i);
-        }
+        HashSet<Integer> hs = new HashSet<>();
+        for(int num : nums) hs.add(num);
         
+        int ans = 0;
         for(int num : nums){
-            recurse(hm, visited, seq, num);
-            ans = Math.max(ans, seq[hm.get(num)]);
+            if(!hs.contains(num)) continue;
+            int left = num - 1, right = num + 1;
+
+            while(hs.contains(left)){
+                hs.remove(left);
+                left--;
+            }
+            
+            while(hs.contains(right)){
+                hs.remove(right);
+                right++;
+            }
+
+            hs.remove(num);
+            ans = Math.max(ans, 1 + (num - left - 1) + (right - num - 1));
         }
 
         return ans;
