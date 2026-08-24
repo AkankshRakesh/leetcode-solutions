5d5b1893c877451b128e632e14708c0b5f57bf14
AkankshRakesh <akankshrakesh@gmail.com>
2026-08-24 14:20:00 +0530

    Time complexity: O(n), Space complexity: O(n)

---
+++ 1872. Stone Game VIII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int[] prefix = new int[stones.length];
+        prefix[0] = stones[0];
+
+        for(int i = 1; i < stones.length; i++){
+            prefix[i] = prefix[i - 1] + stones[i];
+        }
+
+        int ans = prefix[stones.length - 1];
+        for(int i = stones.length - 2; i >= 1; i--){
+            ans = Math.max(ans, prefix[i] - ans);
+        }
+
+        return ans;        
+    }
+}
