a9370443e496dc8b7e883f149ef21dc2cf0ae68a
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-19 22:42:09 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -1,33 +1,37 @@
 class Solution {
-    public int[] expand(String s, int left, int right){
+    public String expand(String s, int left, int right){
+        int index = -1;
+        if(right - left > 1) index = left + 1;
+        StringBuilder sb = new StringBuilder();
+
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            sb.append(s.charAt(right));
             left--;
             right++;
         }
-        return new int[]{left + 1, right - 1};
+
+        StringBuilder ans = new StringBuilder(sb).reverse();
+        if(index != -1) ans.append(s.charAt(index));
+        ans.append(sb);
+
+        return ans.toString();
     }
     public String longestPalindrome(String s) {
-        int len = 0, ansLeft = 0, ansRight = 0;
-        for(int i = 1; i < s.length(); i++){
-            int[] boundary1 = expand(s, i - 1, i + 1);
-            int[] boundary2 = new int[2];
-            if(s.charAt(i) == s.charAt(i - 1)) boundary2 = expand(s, i - 2, i + 1);
-            int len1 = boundary1[1] - boundary1[0];
-            int len2 = boundary2[1] - boundary2[0];
-            if(len < len1 || len < len2){
-                if(len1 < len2){
-                    len = len2;
-                    ansLeft = boundary2[0];
-                    ansRight = boundary2[1];
-                }
-                else{
-                    len = len1;
-                    ansLeft = boundary1[0];
-                    ansRight = boundary1[1];
-                }
+        if(s.length() == 1) return s;
+        String ans = "";
+
+        for(int i = 0; i < s.length() - 1; i++){
+            String evenPal = "";
+            if(s.charAt(i) == s.charAt(i + 1)){
+                evenPal = expand(s, i, i + 1);
             }
+
+            String oddPal = expand(s, i - 1, i + 1);
+
+            if(oddPal.length() > evenPal.length() && oddPal.length() > ans.length()) ans = oddPal;
+            else if(evenPal.length() > ans.length()) ans = evenPal; 
         }
 
-        return s.substring(ansLeft, ansRight + 1);   
+        return ans;
     }
 }
