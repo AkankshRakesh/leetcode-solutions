6bfacc13d65b46f7435354a9563d195a78bf4fb9
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-19 20:22:01 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 560. Subarray Sum Equals K.java
+++ 560. Subarray Sum Equals K.java
@@ -1,24 +1,15 @@
 class Solution {
-    public int subarraySumLessThanEqualToK(int[] nums, int k){
-        int left = 0;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        int ans = 0;
-        int sum = 0;
-        freq.put(0, 1);
-        
-        for(int right = 0; right < nums.length; right++){
-            sum += nums[right];
-            if(freq.containsKey(sum - k)){
-                ans += freq.get(sum - k);
-            }
-            
-            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0, sum = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(hm.containsKey(sum - k)) ans += hm.get(sum - k);
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
         }
-        // System.out.println(ans);
 
         return ans;
-    }
-    public int subarraySum(int[] nums, int k) {
-        return subarraySumLessThanEqualToK(nums, k);
     }
 }
