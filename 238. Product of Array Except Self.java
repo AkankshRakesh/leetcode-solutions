9bf481c8bab99b9cc7b521da8a7a2b9a12111fdb
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-19 19:17:36 +0530

    Time complexity: O(n), Space complexity: O(1)

--- 238. Product of Array Except Self.java
+++ 238. Product of Array Except Self.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
-        int curr = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            curr *= nums[i];
-            ans[i] = curr;
+        ans[0] = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
-        
-        curr = 1;
-        for(int i = 0; i < nums.length; i++){
-            int suffix = i + 1 < nums.length ? ans[i + 1] : 1;
-            curr = i - 1 >= 0 ? curr * nums[i - 1] : curr;
-            ans[i] = curr * suffix;
+
+        int currProduct = nums[nums.length - 1];
+        for(int i = nums.length - 2; i >= 0; i--){
+            ans[i] *= currProduct;
+            currProduct *= nums[i];
         }
+
         return ans;
     }
 }
