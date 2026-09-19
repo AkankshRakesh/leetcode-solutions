168d77754299d5c31c1d812922a4a45eb27d4480
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-19 20:21:38 +0530

    Time complexity: O(n), Space complexity: O(n)

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -2,24 +2,30 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         for(int num : nums) hs.add(num);
-        
         int ans = 0;
-        for(int num : nums){
-            if(!hs.contains(num)) continue;
-            int left = num - 1, right = num + 1;
 
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if(!hs.contains(num)) continue;
+
+            int left = num - 1;
+            int countLeft = 0;
             while(hs.contains(left)){
                 hs.remove(left);
                 left--;
+                countLeft++;
             }
-            
+
+            int right = num + 1;
+            int countRight = 0;
             while(hs.contains(right)){
                 hs.remove(right);
                 right++;
+                countRight++;
             }
 
+            ans = Math.max(ans, countLeft + 1 + countRight);
             hs.remove(num);
-            ans = Math.max(ans, 1 + (num - left - 1) + (right - num - 1));
         }
 
         return ans;
