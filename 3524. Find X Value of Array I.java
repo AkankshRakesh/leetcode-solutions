b566ebff78d9e99a94c92df141d626366102671c
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-21 19:12:41 +0530

    Time complexity: O(n*k), Space complexity: O(n*k)

---
+++ 3524. Find X Value of Array I.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        int n = nums.length;
+        long[][] dp = new long[n][k];
+        dp[0][nums[0] % k] = 1;
+
+        for(int i = 1; i < n; i++) {
+            dp[i][nums[i] % k] = 1;
+            for(int j = 0; j < k; j++)
+                dp[i][(nums[i] % k) * j % k] += dp[i - 1][j];
+        }
+
+        long[] ans = new long[k];
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < n; j++) {
+                ans[i] += dp[j][i];
+            }
+        }
+
+        return ans;
+    }
+}
