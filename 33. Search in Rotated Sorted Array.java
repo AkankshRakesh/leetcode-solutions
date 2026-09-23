ccc5598a8d6c15fe0c8f435fd58aa9527edc3078
AkankshRakesh <akankshrakesh@gmail.com>
2026-09-23 19:30:35 +0530

    Time complexity: O(logn), Space complexity: O(1)

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -1,19 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // if mid < left && mid < right = valid bottom, go left
+        // else go min(left, right)
+
         int left = 0, right = nums.length - 1;
+        int pivotIndex = 0;
         while(left <= right){
             int mid = left + (right - left) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] >= nums[left]){
-                if(target >= nums[left] && target < nums[mid]) right = mid - 1;
-                else left = mid + 1;
-            }
-            else{
-                if(target <= nums[right] && target >= nums[mid]) left = mid + 1;
-                else right = mid - 1;
+            if (nums[mid] > nums[nums.length - 1]) {
+                left = mid + 1;
+            } else {
+                pivotIndex = mid;
+                right = mid - 1;
             }
         }
 
+        // System.out.println(pivotIndex);
+        left = pivotIndex;
+        right = pivotIndex + nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int index = mid % nums.length;
+            
+            if(nums[index] < target) left = mid + 1;
+            else if(nums[index] > target) right = mid - 1;
+            else return index;
+        }
+        
         return -1;
     }
 }
